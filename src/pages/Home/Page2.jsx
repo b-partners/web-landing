7db4094aeca671684394e5a1a61991b5fbfa1972
YumeT023 <hei.hajatiana@gmail.com>
2022-08-06 01:12:28 +0300

    fix: typo

--- src/pages/Home/Page2.jsx
+++ src/pages/Home/Page2.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import myVideo from './assets/img/video.gif';
 
 export function Page2() {
@@ -6,14 +7,14 @@ export function Page2() {
     <section className="share section bd-container" id="share">
       <div className="share__container bd-grid">
         <div className="share__data">
-          <h2 className="section-title-center">
+          <h3 className="section-title-center">
             Bénéficiez de tous les outils d’une banque
             <br />
             traditionnelle adaptés à vos besoins
-          </h2>
+          </h3>
           <p className="share__description">
             <i className="fa fa-check" aria-hidden="true" />
-            &nbsp; Compte pro avec IBAN EE
+            &nbsp; Obtenez votre compte professionnel avec un IBAN/RIB FR en 60 secondes.
           </p>
           <p className="share__description">
             <i className="fa fa-check" aria-hidden="true" />
@@ -22,6 +23,10 @@ export function Page2() {
           <p className="share__description">
             <i className="fa fa-check" aria-hidden="true" />
             &nbsp; Virements instantanés jusqu'a 100 000€
+          </p>
+          <p className="share__description">
+            <i className="fa fa-check" aria-hidden="true" />
+            &nbsp; Une carte bancaire physique ou virtuelle selon vos besoins.
           </p>
         </div>
         <div className="share__img">
