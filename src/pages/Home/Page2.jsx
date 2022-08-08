94c4c4674be5d35ceb41dabfbd895edb3265f7b1
Yume <hei.hajatiana@gmail.com>
2022-08-08 14:29:49 +0300

    feat: add page 2

---
+++ src/pages/Home/Page2.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import myVideo from './assets/img/video.gif';
+
+export function Page2() {
+  return (
+    <section className="share section bd-container" id="share">
+      <div className="share__container bd-grid">
+        <div className="share__data">
+          <h3 className="section-title-center">
+            Bénéficiez de tous les outils d’une banque
+            <br />
+            traditionnelle adaptés à vos besoins
+          </h3>
+          <p className="share__description">
+            <i className="fa fa-check" aria-hidden="true" />
+            &nbsp; Obtenez votre compte professionnel avec un IBAN/RIB FR en 60 secondes.
+          </p>
+          <p className="share__description">
+            <i className="fa fa-check" aria-hidden="true" />
+            &nbsp; Virements SEPA entrants et sortants illimités
+          </p>
+          <p className="share__description">
+            <i className="fa fa-check" aria-hidden="true" />
+            &nbsp; Virements instantanés jusqu'a 100 000€
+          </p>
+          <p className="share__description">
+            <i className="fa fa-check" aria-hidden="true" />
+            &nbsp; Une carte bancaire physique ou virtuelle selon vos besoins.
+          </p>
+        </div>
+        <div className="share__img">
+          <img alt="ok" src={myVideo} />
+        </div>
+      </div>
+    </section>
+  );
+}
