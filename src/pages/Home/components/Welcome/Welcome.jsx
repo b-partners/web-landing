6d72cabd0c855a1fcc5671b7cc895521383fa225
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-08-11 18:31:48 +0300

    chore: editing anchor link

--- src/pages/Home/components/Welcome/Welcome.jsx
+++ src/pages/Home/components/Welcome/Welcome.jsx
@@ -55,7 +55,7 @@ export function Welcome() {
           FR, virement,
           tenu de compte, cartes) offrez-vous l’assistant de votre réussite grâce à votre mobile
         </p>
-        <a href='/src/pages' className='button__register'>
+        <a href='#welcome' className='button__register'>
           Lancement de l’application mobile et web le 1 Novembre 2022 – Inscrivez-vous ici pour devenir
           les premiers
           utilisateurs
