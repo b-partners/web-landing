61f273d1e63f2ec1820adac0d0ec4febea67b886
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-08-11 17:59:46 +0300

    refactor: form handling

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 
 import artisans from './assets/img/artisans.jpg';
 import assistant from './assets/img/home-overview.jpg';
@@ -7,146 +6,133 @@ import './assets/css/modal.css';
 import './assets/css/input.css';
 import './assets/css/spinner.css';
 import { useToggle } from '../../utils/hooks';
+import httpClient from '../../config/axios';
 
 export function HomePage() {
-  const info = {
-    email: '',
-    lastName: '',
-    firstName: '',
-    tel: '',
-  };
-
-  const [modal, toggleModal] = useToggle(false);
-  const [data, setData] = useState(info);
-  const [loading, setLoading] = useState('');
+  const [showModal, setToggleShowModal] = useToggle(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [preregistrationPayload, setPreregistrationPayload] = useState({
+    first_name: '', last_name: '', society: '', email: '', phone_number: '',
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    toggleModal();
+    setToggleShowModal(!showModal);
   };
 
-  const sendData = () => {
-    setLoading('loading');
-    axios.post('https://jsonplaceholder.typicode.com/posts', info)
-      .then((response) => {
-        if (response !== null) {
-          setLoading('');
-          toggleModal();
-        }
-      })
-      .catch((error) => {
-        throw error;
-      });
-  };
-
-  const checkInput = (e) => {
-    if (data.firstName && data.lastName && data.tel !== '') {
-      sendData();
-      e.preventDefault();
+  const sendData = async (payload) => {
+    setIsLoading(true);
+    try {
+      await httpClient.post('/pre-registration', payload);
+    } catch (e) {
+      throw new Error(e);
+    } finally {
+      setIsLoading(false);
     }
+  };
+
+  const handleModalSubmit = async (e) => {
+    e.preventDefault();
+    await sendData({ ...preregistrationPayload });
   };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setData((prevState) => ({ ...prevState, [name]: value }));
+    setPreregistrationPayload((state) => ({ ...state, [name]: value }));
   };
 
-  return (
-    <section className="home" id="home">
-      <div className="home__container bd-container bd-grid">
-        <div className="home__data">
-          <h1 className="home__title">
-            L'assistant bancaire qui accélère le développent des artisans et indépendants.
-          </h1>
-          <div className="home__image">
-            <img alt="logo" src={artisans} style={{ width: '16.875rem' }} />
-          </div>
-          <p className="home__description">
-            Artisans, indépendants français, en plus des services bancaires classiques (obtention d’un RIB FR, virement,
-            tenu de compte, cartes) offrez-vous l’assistant de votre réussite grâce à votre mobile
-          </p>
-          <a href="src/pages/Home/Home#" className="button__registration">
-            Lancement de l’application mobile et web le 1 Novembre 2022 – Inscrivez-vous ici pour devenir les premiers
-            utilisateurs
-          </a>
-          <div className="home__email">
-            <form className="home__email__form" onSubmit={handleSubmit}>
-              <input
-                type="email"
-                name="email"
-                placeholder="Votre email @"
-                className="home__email__input"
-                onChange={handleChange}
-                required
-              />
-            </form>
-          </div>
+  return (<section className='home' id='home'>
+    <div className='home__container bd-container bd-grid'>
+      <div className='home__data'>
+        <h1 className='home__title'>
+          L'assistant bancaire qui accélère le développent des artisans et indépendants.
+        </h1>
+        <div className='home__image'>
+          <img alt='logo' src={artisans} style={{ width: '16.875rem' }} />
+        </div>
+        <p className='home__description'>
+          Artisans, indépendants français, en plus des services bancaires classiques (obtention d’un RIB
+          FR, virement,
+          tenu de compte, cartes) offrez-vous l’assistant de votre réussite grâce à votre mobile
+        </p>
+        <a href='/' className='button__register'>
+          Lancement de l’application mobile et web le 1 Novembre 2022 – Inscrivez-vous ici pour devenir
+          les premiers
+          utilisateurs
+        </a>
+        <div className='home__email'>
+          <form className='home__email__form' onSubmit={handleSubmit}>
+            <input
+              type='email'
+              name='email'
+              placeholder='Votre email @'
+              className='home__email__input'
+              onChange={handleChange}
+              required
+            />
+          </form>
+        </div>
+        <div
+          className={`home__modal ${showModal ? '' : 'modal-hide'}`}
+          onClick={setToggleShowModal}
+          onKeyDown={setToggleShowModal}
+          role='button'
+          tabIndex={0}
+        >
           <div
-            className={`home__modal ${modal ? '' : 'modal-hide'}`}
-            onClick={toggleModal}
-            onKeyDown={toggleModal}
-            role="button"
+            className='modal__wrapper'
+            onClick={(e) => e.stopPropagation()}
+            onKeyDown={(e) => e.stopPropagation()}
+            role='button'
             tabIndex={0}
           >
-            <div
-              className="modal__wrapper"
-              onClick={(e) => e.stopPropagation()}
-              onKeyDown={(e) => e.stopPropagation()}
-              role="button"
-              tabIndex={0}
-            >
-              <form onSubmit={checkInput}>
-                <div className="floating-form">
-                  <div className="floating-label">
-                    <input
-                      className="floating-input"
-                      type="text"
-                      placeholder=" "
-                      name="lastName"
-                      onChange={handleChange}
-                      required
-                    />
-                    <span>Nom</span>
-                  </div>
-                  <div className="floating-label">
-                    <input
-                      className="floating-input"
-                      type="text"
-                      placeholder=" "
-                      name="firstName"
-                      onChange={handleChange}
-                      required
-                    />
-                    <span>Prénom</span>
-                  </div>
-                  <div className="floating-label">
-                    <input
-                      className="floating-input"
-                      type="tel"
-                      placeholder=" "
-                      name="tel"
-                      onChange={handleChange}
-                      required
-                    />
-                    <span>Téléphone</span>
-                  </div>
-                  <button type="button" onClick={checkInput} className="form__validate">
-                    {loading === 'loading' ? (
-                      <>
-                        <i className="fa fa-spinner fa-spin" />
-                        Loading
-                      </>
-                    ) : 'Valider'}
-                  </button>
+            <form onSubmit={handleModalSubmit}>
+              <div className='floating-form'>
+                <div className='floating-label'>
+                  <input
+                    className='floating-input'
+                    type='text'
+                    placeholder=' '
+                    name='last_name'
+                    onChange={handleChange}
+                  />
+                  <span>Nom</span>
                 </div>
-              </form>
-            </div>
+                <div className='floating-label'>
+                  <input
+                    className='floating-input'
+                    type='text'
+                    placeholder=' '
+                    name='first_name'
+                    onChange={handleChange}
+                  />
+                  <span>Prénom</span>
+                </div>
+                <div className='floating-label'>
+                  <input
+                    className='floating-input'
+                    type='text'
+                    placeholder=''
+                    name='phone_number'
+                    onChange={handleChange}
+                    required
+                  />
+                  <span>Téléphone</span>
+                </div>
+                <button type='submit' className='form__validate'>
+                  {isLoading ? (<>
+                    <i className='fa fa-spinner fa-spin' />
+                    Loading
+                  </>) : 'Valider'}
+                </button>
+              </div>
+            </form>
           </div>
         </div>
-        <div className="home__img">
-          <img className="home__img__overview" alt="assistant" src={assistant} style={{ width: '36.25rem' }} />
-        </div>
       </div>
-    </section>
-  );
+      <div className='home__img'>
+        <img className='home__img__overview' alt='assistant' src={assistant} style={{ width: '36.25rem' }} />
+      </div>
+    </div>
+  </section>);
 }
