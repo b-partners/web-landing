fd390b79dc030127bd5bf90e3975f2f6b9b89548
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-08-12 09:51:32 +0300

    chore: replacing button by custom button component

--- src/pages/Home/components/Header/Header.jsx
+++ src/pages/Home/components/Header/Header.jsx
@@ -6,12 +6,14 @@ import '../../assets/css/input.css';
 import { useToggle } from '../../../../utils/hooks';
 import { Modal } from '../../../../common/components/Modal';
 import httpClient from '../../../../config/axios';
+import { Button } from '../../../../common/components/Button';
 
 export function Header() {
   const [menuActive, toggleMenuActive] = useToggle(false);
   const buttonStyle = { backgroundColor: 'transparent', border: 'none' };
   const [showModal, setToggleShowModal] = useToggle(false);
   const [authPayload, setAuthPayload] = useState({ phoneNumber: '' });
+  const [isLoading, setIsLoading] = useState(false);
 
   const handlePhoneNumberChange = event => {
     const { name, value } = event.target;
@@ -20,65 +22,68 @@ export function Header() {
 
   const handleSubmit = async event => {
     event.preventDefault();
+    setIsLoading(true);
     try {
       const { code } = await httpClient.post(`auth`, authPayload);
-      const response  = await httpClient.post(`token`, { code });
+      const response = await httpClient.post(`token`, { code });
       console.log(response, code);
     } catch (e) {
       throw new Error(e);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (<header className='l-header' id='header'>
-      <nav className='nav bd-container'>
-        <a href='#home' className='nav__logo'>
-          <img src={logo} alt='logo' style={{ width: '10.625rem' }} />
-        </a>
-        <div className={`nav__menu ${menuActive ? 'show-menu' : ''}`} id='nav-menu'>
-          <ul className='nav__list'>
-            <li className='nav__item'>
-              <a href='#home' className='nav__link active-link'>
-                Accueil
-              </a>
-            </li>
-            <li className='nav__item'>
-              <a href='#share' className='nav__link'>
-                Fonctionnalités
-              </a>
-            </li>
-            <li className='nav__item'>
-              <button type='button' className='nav__link' style={buttonStyle} onClick={() => setToggleShowModal(true)}>
-                Se connecter
-              </button>
-            </li>
-            <li className='nav__item' id='ouvrir-compte'>
-              <button type='button' className='nav__link' style={{ color: 'white', ...buttonStyle }}>
-                Ouvrir un compte
-              </button>
-            </li>
-          </ul>
-        </div>
-        <button className='nav__toggle toggle-wrapper' id='nav-toggle' onClick={toggleMenuActive} type='button'>
-          <i className='bx bx-grid-alt' />
-        </button>
-      </nav>
-      <Modal showModal={showModal} toggle={setToggleShowModal}>
-        <form onSubmit={handleSubmit}>
-          <div className='floating-form'>
-            <div className='floating-label'>
-              <input
-                className='floating-input'
-                type='text'
-                placeholder=''
-                name='phoneNumber'
-                onChange={handlePhoneNumberChange}
-                required
-              />
-              <span>Téléphone</span>
-            </div>
-            <button type='submit' className='btn btn-primary'>Se connecter</button>
+    <nav className='nav bd-container'>
+      <a href='#home' className='nav__logo'>
+        <img src={logo} alt='logo' style={{ width: '10.625rem' }} />
+      </a>
+      <div className={`nav__menu ${menuActive ? 'show-menu' : ''}`} id='nav-menu'>
+        <ul className='nav__list'>
+          <li className='nav__item'>
+            <a href='#home' className='nav__link active-link'>
+              Accueil
+            </a>
+          </li>
+          <li className='nav__item'>
+            <a href='#share' className='nav__link'>
+              Fonctionnalités
+            </a>
+          </li>
+          <li className='nav__item'>
+            <button type='button' className='nav__link' style={buttonStyle} onClick={() => setToggleShowModal(true)}>
+              Se connecter
+            </button>
+          </li>
+          <li className='nav__item' id='ouvrir-compte'>
+            <button type='button' className='nav__link' style={{ color: 'white', ...buttonStyle }}>
+              Ouvrir un compte
+            </button>
+          </li>
+        </ul>
+      </div>
+      <button className='nav__toggle toggle-wrapper' id='nav-toggle' onClick={toggleMenuActive} type='button'>
+        <i className='bx bx-grid-alt' />
+      </button>
+    </nav>
+    <Modal showModal={showModal} toggle={setToggleShowModal}>
+      <form onSubmit={handleSubmit}>
+        <div className='floating-form'>
+          <div className='floating-label'>
+            <input
+              className='floating-input'
+              type='text'
+              placeholder=''
+              name='phoneNumber'
+              onChange={handlePhoneNumberChange}
+              required
+            />
+            <span>Téléphone</span>
           </div>
-        </form>
-      </Modal>
-    </header>);
+          <Button type='submit' loading={isLoading} label='Se connecter' />
+        </div>
+      </form>
+    </Modal>
+  </header>);
 }
