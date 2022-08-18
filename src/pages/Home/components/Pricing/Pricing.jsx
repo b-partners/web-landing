9fa9daf4e77dbfd75aa20ac988044d8a007f38db
Rojotiana Nomena <noumsrafalimanana26@gmail.com>
2022-08-18 11:52:04 +0300

    Fix/pricing (#24)
    * fix: wording
    
    * fix: pricing design

--- src/pages/Home/components/Pricing/Pricing.jsx
+++ src/pages/Home/components/Pricing/Pricing.jsx
@@ -8,8 +8,6 @@ import connexion from '../../assets/img/connexion.png';
 import confirmer from '../../assets/img/identity.png';
 import valider from '../../assets/img/valider.png';
 
-import { ReactComponent as Apple } from '../../assets/img/apple-store.svg';
-import { ReactComponent as Google } from '../../assets/img/google-store.svg';
 import { Preregistration } from '../Preregistration';
 import { usePreregistration } from '../../../../utils/hooks';
 
@@ -17,14 +15,14 @@ export function Pricing() {
   const { isLoading, showModal, toggle, handleEmailSubmit, handleModalSubmit, handleChange } = usePreregistration();
 
   return (
-    <div className="pricing__container">
+    <div className='pricing__container'>
       <div className="pricing__main">
         <button type='button' className="button button--pricing">
           Préinscrivez-vous et bénéficiez de la formule de votre choix gratuite pendant 1 an (offre limitée au 500
           premiers clients)
         </button>
         <Preregistration onSubmitEmail={handleEmailSubmit} onChange={handleChange} showModal={showModal} toggle={toggle}
-                         onSubmitModal={handleModalSubmit} loading={isLoading} />
+                          onSubmitModal={handleModalSubmit} loading={isLoading} />
         <div className="background">
           <div className="container">
             <div className="pricing-table">
@@ -50,11 +48,11 @@ export function Pricing() {
                   </li>
                   <li className="pricing-features-item">
                     <strong>
-                      Activation de la version standard de notre assistant virtuelle pour faire grossir votre activité.
+                      Activation de la version standard de notre assistant virtuel pour faire grossir votre activité.
                     </strong>
                   </li>
                   <li className="pricing-features-item">
-                    <strong>Service de banque en ligne (RIB/Virement/carte à partir de 2€/mois).</strong>
+                    <strong>Service de banque en ligne (RIB/Virements/cartes à partir de 2€/mois).</strong>
                   </li>
                   <li className="pricing-features-item">
                     <strong>
@@ -88,7 +86,7 @@ export function Pricing() {
                   </li>
                   <li className="pricing-features-item">
                     Support &nbsp;
-                    <strong>7 jours sur 7</strong>
+                    <strong>7 jours sur 7.</strong>
                   </li>
                 </ul>
               </div>
@@ -115,11 +113,11 @@ export function Pricing() {
                   </li>
                   <li className="pricing-features-item">
                     <strong>
-                      Activation de la version standard de l'assistant virtuelle pour faire grossir votre activité.
+                      Activation de la version standard de l'assistant virtuel pour faire grossir votre activité.
                     </strong>
                   </li>
                   <li className="pricing-features-item">
-                    <strong>Service de banque en ligne (RIB/Virement/carte à partir de 2€/mois).</strong>
+                    <strong>Service de banque en ligne (RIB/Virements/cartes à partir de 2€/mois).</strong>
                   </li>
                   <li className="pricing-features-item">
                     <strong>
@@ -149,7 +147,7 @@ export function Pricing() {
                   </li>
                   <li className="pricing-features-item">
                     Support &nbsp;
-                    <strong>7 jours sur 7</strong>
+                    <strong>7 jours sur 7.</strong>
                   </li>
                 </ul>
               </div>
@@ -177,16 +175,16 @@ export function Pricing() {
                   </li>
                   <li className="pricing-features-item">
                     <strong>
-                      Activation de la version personnalisée de notre assistant virtuelle pour faire grossir votre
+                      Activation de la version personnalisée de notre assistant virtuel pour faire grossir votre
                       activité.
                     </strong>
                   </li>
                   <li className="pricing-features-item">
-                    <strong>Service de banque en ligne (RIB/Virement/carte à partir de 2€/mois).</strong>
+                    <strong>Service de banque en ligne (RIB/Virements/cartes à partir de 2€/mois).</strong>
                   </li>
                   <li className="pricing-features-item">
                     <strong>
-                      Bénéficiez de notre outil de devis, facturation, comptabilité automatisée (multi utilisateur).
+                      Bénéficiez de notre outil de devis, facturation, comptabilité automatisée (multi utilisateurs).
                     </strong>
                   </li>
                   <li className="pricing-features-item">
@@ -210,6 +208,10 @@ export function Pricing() {
                     <strong>100</strong>
                     &nbsp; virements et prélèvements puis 0,20€ au delà.
                   </li>
+                  <li className="pricing-features-item">
+                    Support &nbsp;
+                    <strong>7 jours sur 7.</strong>
+                  </li>
                 </ul>
               </div>
             </div>
@@ -217,46 +219,29 @@ export function Pricing() {
         </div>
       </div>
 
-      <div className="pricing__section">
-        <section className="decoration" id="decoration">
-          <h3 className="decoration-title">Comment ouvrir un compte ?</h3>
-          <div className="decoration__container">
-            <div className="decoration__data">
-              <img src={connexion} alt="" className="step-image" />
-              <h4 className="step-text">Ouvrez votre compte en moins de 5 minutes sans paperasse</h4>
-            </div>
+      <section className="section decoration" id="decoration">
+        <h3 className="decoration-title">Comment ouvrir un compte ?</h3>
+        <div className="decoration__container">
+          <div className="decoration__data">
+            <img src={connexion} alt="" className="step-image" />
+            <h4 className="step-text">Ouvrez votre compte en moins de 5 minutes sans paperasse</h4>
+          </div>
 
-            <div className="decoration__data">
-              <img src={confirmer} alt="" className="step-image2" />
-              <h4 className="step-text">Confirmez votre identité avec un selfie et une pièce d’identité</h4>
-            </div>
+          <div className="decoration__data">
+            <img src={confirmer} alt="" className="step-image2" />
+            <h4 className="step-text">Confirmez votre identité avec un selfie et une pièce d’identité</h4>
+          </div>
 
-            <div className="decoration__data">
-              <img src={valider} alt="" className="step-image2" />
-              <h4 className="step-text">
-                Votre compte est validé &nbsp;
-                <br />
-                et votre IBAN prêt
-              </h4>
-            </div>
+          <div className="decoration__data">
+            <img src={valider} alt="" className="step-image2" />
+            <h4 className="step-text">
+              Votre compte est validé &nbsp;
+              <br />
+              et votre IBAN prêt
+            </h4>
           </div>
-        </section>
-        <section className="send">
-          <h2 className="decoration-title">Ouvrir un compte dès maintenant</h2>
-          <div className="texte">30 jours gratuits et sans engagement, à partir de 9€ par mois.</div>
-          <div className="compte">
-            <Apple alt="" class="dl" />
-            <Google alt="" class="dl" />
-            <br />
-          </div>
-          <div className="texte2">
-            <p>⭐ ⭐ ⭐ ⭐ ⭐ 4.8/5</p>
-          </div>
-          <div className="texte3">
-            <p>Note basée sur + de 18 200 avis</p>
-          </div>
-        </section>
-      </div>
+        </div>
+      </section>
     </div>
   );
 }
