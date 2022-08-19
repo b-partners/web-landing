56502107d2a4f4af7c6efcea818a0f9ad9e66015
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-08-19 10:41:33 +0300

    feat: adding modal on opening account button

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,12 @@ import { Welcome } from './components/Welcome';
 import { Pricing } from './components/Pricing';
 
 export function Home() {
+  const [showModal, setToggleShowModal] = useToggle(false);
+
+  const activeModal = (e) => {
+    e.preventDefault();
+    setToggleShowModal(true);
+  };
   return (
     <div>
       <Header />
