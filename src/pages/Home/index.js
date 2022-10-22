e96cec22d8fa8875a2f0fa6bca186a17aefdeec9
Ny Hasinavalona Randriantsarafara <94039290+hei-nyhasina@users.noreply.github.com>
2022-10-22 21:36:09 +0300

    chore: removing unused components

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,3 @@
 export * from './Home';
-export { Preregistration } from './components/Preregistration';
 export { PreRegistrationModal } from './components/PreRegistrationModal/PreRegistrationModal';
 export { usePreRegistration } from './components/PreRegistrationModal/PreRegistrationModal';
