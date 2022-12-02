af8f07309812b2aed63cc9af2027a7b1d0cd550f
Judicael <99041636+JrManjato@users.noreply.github.com>
2022-12-02 13:53:58 +0300

    chore: change text on feature section

--- src/pages/Home/components/Feature/Feature.jsx
+++ src/pages/Home/components/Feature/Feature.jsx
@@ -7,7 +7,7 @@ export function Feature() {
     <section className="share" id="share">
       <div className="share__container bd-container">
         <ShareCard cardText="Obtenez votre compte professionnel avec un IBAN/RIB FR en quelques secondes." />
-        <ShareCard cardText="Virements et encaissements instantanés." />
+        <ShareCard cardText="Initiation des virements et des encaissements instantanés." />
         <ShareCard cardText="Une carte physique ou virtuelle selon vos besoins." />
         <ShareCard cardText="Agrégez vos comptes pour analyser vos performances." />
       </div>
