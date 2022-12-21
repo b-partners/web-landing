8f7122dac6c8c49b5976cae59e0bfb59a73f505e
Judicael <99041636+JrManjato@users.noreply.github.com>
2022-12-21 09:55:23 +0300

    chore: change the order of the components (feature and authencity)

--- src/pages/Home/components/Feature/Feature.jsx
+++ src/pages/Home/components/Feature/Feature.jsx
@@ -1,32 +1,13 @@
 import React from 'react';
 
-import { ShareCard } from '../../../../common/components/ShareCard';
-import Bank from '../../assets/img/bank.png';
-import Euro from '../../assets/img/euro.png';
-import Wallet from '../../assets/img/wallet.png';
-import Trade from '../../assets/img/trade.png';
-
 export function Feature() {
   return (
     <section className="share" id="share">
-      <div className="share__container  bd-container">
-        <ShareCard
-          cardText="Obtenez votre compte professionnel avec un IBAN/RIB FR en quelques secondes."
-          imgSrc={Bank}
-        />
-        <ShareCard
-          cardText="Initiez vos virements et vos encaissements en 1 clic."
-          imgSrc={Euro}
-        />
-        <ShareCard
-          cardText="Obtenez votre carte physique ou virtuelle selon vos besoins."
-          imgSrc={Wallet}
-        />
-        <ShareCard
-          cardText="Agrégez vos comptes pour analyser vos performances."
-          imgSrc={Trade}
-        />
-      </div>
+      <h2 className='mobile__title'>
+        <b className='text__highlight'>Votre mobile s'occupe de gérer les <br />tâches administratives,
+          financières et <br />commerciales  </b>
+        pendant que vous faites <br />votre métier d'artisan.
+      </h2>
     </section>
   );
 }
