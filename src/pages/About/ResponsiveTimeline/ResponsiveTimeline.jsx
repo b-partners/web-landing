0f80255b633c034d556bbf16f5bd5ef2b656c131
Judicael <99041636+JrManjato@users.noreply.github.com>
2023-01-10 17:26:39 +0300

    chore: change the text in the timeline (#65)

--- src/pages/About/ResponsiveTimeline/ResponsiveTimeline.jsx
+++ src/pages/About/ResponsiveTimeline/ResponsiveTimeline.jsx
@@ -12,25 +12,25 @@ export function ResponsiveTimeline() {
                 preset='responsive-width'
             />
             <Polygone
-                description='Lancement des rencontres avec les artisans en S2 2022.'
+                description='Création de la société en Juillet 2022, lancement des développements IT.'
                 color='rgba(165, 58, 106, 1)'
                 position='polygone-right'
                 preset='responsive-width'
             />
             <Polygone
-                description='Lancement des rencontres avec les artisans en S2 2022.'
+                description='Interviews des artisans testeurs.'
                 color='rgba(175, 79, 122, 1)'
                 position='polygone-right'
                 preset='responsive-width'
             />
             <Polygone
-                description='Lancement des rencontres avec les artisans en S2 2022.'
+                description='Novembre 2022 : Commercialisation.'
                 color='rgba(197, 129, 161, 1)'
                 position='polygone-right'
                 preset='responsive-width'
             />
             <Polygone
-                description='Lancement des rencontres avec les artisans en S2 2022.'
+                description='Décembre 2022 : Lancement d’un programme de R&D.'
                 color='rgba(213, 164, 188, 1)'
                 position='polygone-right'
                 borderLeft='polygone-border-right'
