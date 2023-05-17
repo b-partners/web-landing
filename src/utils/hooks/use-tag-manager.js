001873b7acd1647ce8374b216173d6fcf52ba075
Judicael <judi.randriamanjato@gmail.com>
2023-05-17 16:19:32 +0300

    feat: add clickClass attribut as variable in gtm

--- src/utils/hooks/use-tag-manager.js
+++ src/utils/hooks/use-tag-manager.js
@@ -1,5 +1,8 @@
 const tagManager = (eventName) => {
-    window.dataLayer.push({event: 'buttonClick', eventName});
+    window.dataLayer.push({
+        'event': 'click',
+        'clickClass': eventName,
+    });
 };
 
 export const useTagManager = () => tagManager;
