bfeb56ddc48b5ddd4396df177c19d7e882b2874c
amourRamanantsiresy <hei.amour.2@gmail.com>
2023-07-20 15:40:16 +0300

    chore: Auvalie to BRED

--- src/common/components/ImageCard/ImageCard.jsx
+++ src/common/components/ImageCard/ImageCard.jsx
@@ -1,31 +1,33 @@
 import React from 'react';
-import {LazyLoadImage} from 'react-lazy-load-image-component';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
 import PropTypes from 'prop-types';
 
-export function ImageCard({imageSrc, alt, cardText, styles}) {
-    return (
-        <div className="distinction__item" style={styles}>
-            <LazyLoadImage
-                src={imageSrc}
-                height={120}
-                PlaceholderSrc={alt}
-                effect="blur"
-            />
-            <p style={{width: '75%'}}>{cardText}</p>
-        </div>
-    );
+export function ImageCard({ imageSrc, alt, cardText, styles }) {
+  return (
+    <div className="distinction__item" style={styles}>
+      <LazyLoadImage
+        src={imageSrc}
+        height={120}
+        PlaceholderSrc={alt}
+        effect="blur"
+        style={{ objectFit: 'contain', maxWidth: '160px' }}
+      />
+      <p style={{ width: '75%' }}>{cardText}</p>
+    </div>
+  );
 }
 
 ImageCard.propTypes = {
-    imageSrc: PropTypes.string,
-    alt: PropTypes.string,
-    cardText: PropTypes.string,
-    styles: PropTypes.objectOf(),
+  imageSrc: PropTypes.string,
+  alt: PropTypes.string,
+  cardText: PropTypes.string,
+  styles: PropTypes.objectOf(),
 };
 
 ImageCard.defaultProps = {
-    imageSrc: 'fa fa-bullseye fa-2x',
-    alt: 'fa fa-bullseye fa-2x',
-    cardText: '',
-    styles: {}
+  imageSrc: 'fa fa-bullseye fa-2x',
+  alt: 'fa fa-bullseye fa-2x',
+  cardText: '',
+  styles: {},
 };
