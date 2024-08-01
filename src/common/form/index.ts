14b727777cdf35989e5dcfbcc1123ae1b462355e
amourRamanantsiresy <hei.amour.2@gmail.com>
2024-08-01 16:04:19 +0300

    chore: update preregistation form

--- src/common/form/index.ts
+++ src/common/form/index.ts
@@ -1,1 +1,2 @@
 export * from './pre-registration';
+export * from './utils';
--- src/common/form/pre-registration.ts
+++ src/common/form/pre-registration.ts
@@ -3,28 +3,18 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import zod from 'zod';
 
+import { passValidation, phoneValidator, requiredString } from './utils';
+
 const preRegistrationSchema = zod.object({
-  firstName: zod.string({ message: 'Ce champ est requis' }).min(1, 'Ce champ est requis'),
-  lastName: zod.string({ message: 'Ce champ est requis' }).min(1, 'Ce champ est requis'),
-  email: zod
-    .string({ message: 'Ce champ est requis' })
-    .min(1, 'Ce champ est requis')
-    .email({ message: 'Adresse email invalide' }),
-  phone: zod.custom((value: string) => {
-    if ((value?.length || 0) > 0) {
-      return 'Ce champ est requis.';
-    }
-    const pattern = /^\+?\d+$/;
-    if (!pattern.test(value)) {
-      return 'Numéro de téléphone invalide.';
-    }
-    return true;
-  }),
-  society: zod.custom(() => true),
+  firstName: requiredString(),
+  lastName: requiredString(),
+  email: requiredString().email({ message: 'Adresse email invalide' }),
+  phone: phoneValidator(),
+  society: passValidation(),
 });
 
 const preRegistrationResolver = zodResolver(preRegistrationSchema);
-type TPreRegistration = zod.infer<typeof preRegistrationSchema>;
+export type TPreRegistration = zod.infer<typeof preRegistrationSchema>;
 
 const defaultValues: TPreRegistration = {
   email: '',
