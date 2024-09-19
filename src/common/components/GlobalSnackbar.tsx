23ea8fef56d8256d7c772bf53cd5a23fe384cc8b
RickaPrincy <rckprincy@gmail.com>
2024-09-19 10:37:59 +0300

    fix: snackBar notification

--- src/common/components/GlobalSnackbar.tsx
+++ src/common/components/GlobalSnackbar.tsx
@@ -1,7 +1,13 @@
-import { Snackbar } from '@mui/material';
+import { Alert, Snackbar } from '@mui/material';
 import { useSnackbar } from '@store/snackbar';
 
 export function GlobalSnackbar() {
-  const { isOpen, close, message } = useSnackbar();
-  return <Snackbar autoHideDuration={5000} open={isOpen} onClose={close} message={message} />;
+  const { isOpen, close, message, type } = useSnackbar();
+  return (
+    <Snackbar anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }} autoHideDuration={5000} open={isOpen} onClose={close}>
+      <Alert severity={type} onClose={close} variant="filled" sx={{ width: '100%' }}>
+        {message}
+      </Alert>
+    </Snackbar>
+  );
 }
