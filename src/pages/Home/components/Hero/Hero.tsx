7564ecc21a14c4c84b30334740361e7c20efff06
Ricka Princy <127594276+RickaPrincy@users.noreply.github.com>
2025-01-21 16:38:08 +0300

    feat: rdv conversion tag

--- src/pages/Home/components/Hero/Hero.tsx
+++ src/pages/Home/components/Hero/Hero.tsx
@@ -1,8 +1,10 @@
 import { useNavigate } from 'react-router-dom';
 
 import { BpButton } from '@/common/components/Button';
+import { BP_COLOR } from '@/config';
 import { Box, Container, Stack, Typography } from '@mui/material';
 
+import '../../assets/css/hero.css';
 import { HeroPageStyle } from './style';
 
 export function Hero() {
@@ -22,33 +24,54 @@ export function Hero() {
             l'état des toitures et végétations, la non-conformité d’un territoire ou les risques de sinistres facilitant ainsi la prise de décision.
           </span>
         </Typography>
-        <Stack direction="row" spacing={2}>
-          <BpButton
-            onClick={() => {
-              navigate('/craftsman');
+        <Stack direction="column" sx={{ width: '100%', justifyContent: 'center' }}>
+          <Stack direction="row" sx={{ justifyContent: 'center' }} spacing={2}>
+            <BpButton
+              onClick={() => {
+                navigate('/craftsman');
+              }}
+              sx={{ px: 5, py: 2, fontSize: 16 }}
+            >
+              Couvreurs
+            </BpButton>
+            <BpButton
+              onClick={() => {
+                navigate('/collectivity');
+              }}
+              variant="outlined"
+              sx={{ px: 5, py: 2, fontSize: 16 }}
+            >
+              Collectivités
+            </BpButton>
+            <BpButton
+              onClick={() => {
+                navigate('/insurance');
+              }}
+              variant="outlined"
+              sx={{ px: 5, py: 2, fontSize: 16 }}
+            >
+              Assurances
+            </BpButton>
+          </Stack>
+          <a
+            className="home-subscription-btn"
+            href="https://meet.brevo.com/bpartnersartisans/reunion-de-15-minutes"
+            rel="noreferrer"
+            target="_blank"
+            style={{
+              backgroundColor: BP_COLOR['5'],
+              margin: '20px auto',
+              padding: '18px 0px',
+              width: '90%',
+              color: 'white',
+              borderRadius: '30px',
+              maxWidth: '500px',
+              opacity: 0.9,
+              transition: 'all linear .5s',
             }}
-            sx={{ px: 5, py: 2, fontSize: 16 }}
           >
-            Couvreurs
-          </BpButton>
-          <BpButton
-            onClick={() => {
-              navigate('/collectivity');
-            }}
-            variant="outlined"
-            sx={{ px: 5, py: 2, fontSize: 16 }}
-          >
-            Collectivités
-          </BpButton>
-          <BpButton
-            onClick={() => {
-              navigate('/insurance');
-            }}
-            variant="outlined"
-            sx={{ px: 5, py: 2, fontSize: 16 }}
-          >
-            Assurances
-          </BpButton>
+            RÉSERVER MA DÉMO
+          </a>
         </Stack>
       </Stack>
       <Box className="hero-images">
