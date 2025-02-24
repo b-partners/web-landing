e0d230231905b7364de71ca7f00a5fde715368a2
amourRamanantsiresy <hei.amour.2@gmail.com>
2025-02-24 11:13:17 +0300

    feat: create advertising route

---
+++ src/pages/Advertising-Campaign/AdvertisingCampaign.tsx
@@ -0,0 +1,9 @@
+import { Hero } from './Hero';
+
+export const AdvertisingCampaign = () => {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+};
---
+++ src/pages/Advertising-Campaign/Hero.tsx
@@ -0,0 +1,7 @@
+export const Hero = () => {
+  return (
+    <div>
+      <img alt="Bp IA Logo" src="/assets/images/bp-ia-logo.png" />
+    </div>
+  );
+};
