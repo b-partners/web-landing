cd1c398a6b370ba869a34a430a96d57319af500a
RickaPrincy <rckprincy@gmail.com>
2025-05-09 19:08:10 +0300

    chore: navbar links

--- src/common/components/navbar/utils/constants.ts
+++ src/common/components/navbar/utils/constants.ts
@@ -1,8 +1,8 @@
 export const LINKS = [
   { to: '/home', label: 'Accueil' },
-  { to: '/craftsman', label: 'Couvreur' },
-  { to: '/collectivity', label: 'Collectivité' },
-  { to: '/insurance', label: 'Assurance' },
+  { to: '/craftsman', label: 'Couvreurs' },
+  { to: '/collectivity', label: 'Collectivités' },
+  { to: '/insurance', label: 'Assurances' },
   { to: '/about', label: 'À propos' },
   { to: '/contact', label: 'Contact' },
   { to: 'https://birdia.info', label: 'Ressources' },
