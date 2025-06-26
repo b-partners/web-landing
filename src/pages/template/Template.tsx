1fb2dbed57dcb78c9cdc8b3fdc5eae380382daa3
Swonath <lanathaud@gmail.com>
2025-06-26 15:25:31 +0200

    feat: created new components page template

--- src/pages/template/Template.tsx
+++ src/pages/template/Template.tsx
@@ -1,12 +1,15 @@
 import { Box } from '@mui/material';
 
 import { HeroPage, TheyTrushUs } from './sections';
+import { Analayse, Testimonials } from '@pages/home/components';
 
 export function Template() {
   return (
     <Box className="template">
       <HeroPage />
       <TheyTrushUs />
+      <Analayse />
+      <Testimonials />
     </Box>
   );
 }
