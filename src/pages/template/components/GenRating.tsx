2177b658a08626fedf5067860a0d938ea0a2eabc
RAMANANTSIRESY Amour Bien Aimé <95756837+AmourRamanantsiresy@users.noreply.github.com>
2025-07-10 14:45:59 +0300

    fix: template generator

--- src/pages/template/components/GenRating.tsx
+++ src/pages/template/components/GenRating.tsx
@@ -1,14 +1,16 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { useFormContext } from 'react-hook-form';
 
 import { Rating, RatingProps } from '@mui/material';
 
 export const GenRating: FC<RatingProps> = (props) => {
   const { setValue, getValues } = useFormContext();
+  const [star, setStar] = useState(getValues(props.name));
 
   const onChange: RatingProps['onChange'] = (_event, value) => {
+    setStar(value);
     setValue(props.name, value);
   };
 
-  return <Rating value={getValues(props.name)} max={5} sx={{ mb: 2 }} onChange={onChange} {...props} />;
+  return <Rating value={star} max={5} sx={{ mb: 2 }} onChange={onChange} {...props} />;
 };
