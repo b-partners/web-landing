c80e6bdf2e4d266341b44ed77bf5eb5c898f51b3
AmourRamanantsiresy <hei.amour.2@gmail.com>
2025-07-10 14:04:07 +0000

    Add page /rennes with JSON content

---
+++ src/pages/template/json-data/rennes.ts
@@ -0,0 +1,49 @@
+export default {
+  route: 'paris',
+  hero: {
+    title: 'BIRDIA, Rennes : votre outil intelligent pour diagnostiquer les toitures',
+    items: ['Pas besoin de se déplacer à Rennes', '14 jours d’essai gratuit', 'Jusqu’à 38 diagnostics de toitures inclus'],
+    image: 'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/1d5e020b-c8f8-43b0-b8c9-dbbd7426e0b1',
+  },
+  testimonial: { title1: 'Témoignages clients Paris', title2: "Passez à l'analyse intelligente à Paris" },
+  testimonials: [
+    { name: 'Laurent M', star: 4, content: "Depuis petit j'adore Birdia", job: 'Peintre' },
+    { star: 2, name: 'M Laurent', content: "Depuis petit j'adore Birdia Paris", job: 'Peintre Paris' },
+  ],
+  analyse: {
+    information: [
+      { title: 'Paris', description: 'Paris description' },
+      { title: 'Paris', description: 'Paris description' },
+      { title: 'Paris', description: 'Paris description' },
+    ],
+    explication: ' Description analyse automatisé à Paris',
+  },
+  theyTrustUs: { title: 'A Paris ils nous font confiance' },
+  analayse: { title: 'Analyse automatisé à Paris' },
+  faq: {
+    title1: 'FAQ Paris',
+    q1: { question: 'Paris question 17', answer: 'Paris réponse 1' },
+    q2: { question: 'Paris question 2', answer: 'Paris réponse 2' },
+    q3: { question: 'Paris question 3', answer: 'Paris réponse 3' },
+    q4: { question: 'Paris question 4', answer: 'Paris réponse 4' },
+    q5: { question: 'Paris question 5', answer: 'Paris réponse 5' },
+    q6: { question: 'Paris question 6', answer: 'Paris réponse 6' },
+    title2: 'Vous êtes particulier à Paris ?',
+  },
+  roofDiagnostics: { title: 'Diagnostique Paris title', description1: 'Description Paris 1', description2: 'Description Paris 2' },
+  footer: {
+    title: 'Votre analyse Paris',
+    description: "Votre description d'analyse Paris",
+    link: [
+      { name: 'paris', route: '/paris' },
+      { name: 'Lille', route: '/lille' },
+      { name: 'Lyon', route: '/Lyon' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+    ],
+  },
+};
