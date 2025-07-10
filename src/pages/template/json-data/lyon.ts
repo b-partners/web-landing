241703346b4d8c9c307e800a66d5f04de671534e
AmourRamanantsiresy <hei.amour.2@gmail.com>
2025-07-10 10:35:04 +0000

    Add page /lyon with JSON content

---
+++ src/pages/template/json-data/lyon.ts
@@ -0,0 +1,61 @@
+export default {
+  route: 'paris',
+  hero: {
+    title: 'BIRDIA Lyon, le seul outil de diagnostique toiture',
+    items: ['Pas de CB requise pour Lyon', 'Essaie gratuit de 14 jours Lyon', "Jusqu'à 38 analyses Lyon"],
+    image: 'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/54fa4e74-7150-4903-b48f-b842e04e995a',
+  },
+  testimonial: { title1: 'Témoignages clients Lyon', title2: "Passez à l'analyse intelligente à Lyon" },
+  testimonials: [
+    { name: 'Laurent M Lyon', star: 4, content: "Depuis petit j'adore Lyon", job: 'Peintre Lyon' },
+    { star: 2, name: 'M Laurent Lyon', content: "Depuis petit j'adore Birdia Lyon", job: 'Peintre Paris Lyon' },
+  ],
+  analyse: {
+    information: [
+      { title: 'Paris', description: 'Paris description' },
+      { title: 'Paris', description: 'Paris description' },
+      { title: 'Paris', description: 'Paris description' },
+    ],
+    explication: ' Description analyse automatisé à Lyon',
+    image: 'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/c91e9e27-32e3-4d46-9a0e-922cc2fb63f8',
+  },
+  theyTrustUs: {
+    title: 'A Paris ils nous font confiance',
+    image: [
+      'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/4548fe35-ffdc-4766-a630-906e1ef76391',
+      'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/8438290d-9ebe-443a-8c73-8cf5df103f9e',
+    ],
+  },
+  analayse: { title: 'Analyse automatisé à Lyon' },
+  faq: {
+    title1: 'FAQ Lyon',
+    q1: { question: 'Lyon question 17', answer: 'Lyon réponse 1' },
+    q2: { question: 'Paris question 2', answer: 'Paris réponse 2' },
+    q3: { question: 'Paris question 3', answer: 'Paris réponse 3' },
+    q4: { question: 'Paris question 4', answer: 'Paris réponse 4' },
+    q5: { question: 'Paris question 5', answer: 'Paris réponse 5' },
+    q6: { question: 'Paris question 6', answer: 'Paris réponse 6' },
+    title2: 'Vous êtes particulier à Lyon ?',
+  },
+  roofDiagnostics: {
+    title: 'Diagnostique Lyon title',
+    description1: 'Description Lyon 1',
+    description2: 'Description Lyon 2',
+    image: 'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/fd86f5c6-206c-4dd8-8ea8-7b21e536ef12',
+  },
+  footer: {
+    title: 'Votre analyse Lyon',
+    description: "Votre description d'analyse Lyon",
+    link: [
+      { name: 'paris', route: '/paris' },
+      { name: 'Lille', route: '/lille' },
+      { name: 'Lyon', route: '/Lyon' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+    ],
+  },
+};
