de7d91ad545b8872924fba051d01fede779f7d69
AmourRamanantsiresy <hei.amour.2@gmail.com>
2025-07-10 14:32:17 +0000

    Add page /lille with JSON content

---
+++ src/pages/template/json-data/lille.ts
@@ -0,0 +1,48 @@
+export default {
+  route: 'paris',
+  hero: {
+    title: 'BIRDIA Paris, le seul outil de diagnostique toiture',
+    items: ['Pas de CB requise pour paris', 'Essaie gratuit de 14 jours', "Jusqu'à 38 analyses"],
+  },
+  testimonial: { title1: 'Témoignages clients Paris', title2: "Passez à l'analyse intelligente à Paris" },
+  testimonials: [
+    { name: 'Laurent M', star: 4, content: "Depuis petit j'adore Birdia", job: 'Peintre' },
+    { star: 2, name: 'M Laurent', content: "Depuis petit j'adore Birdia Paris", job: 'Peintre Paris' },
+  ],
+  analyse: {
+    information: [
+      { title: 'Paris', description: 'Paris description' },
+      { title: 'Paris', description: 'Paris description' },
+      { title: 'Paris', description: 'Paris description' },
+    ],
+    explication: ' Description analyse automatisé à Paris',
+  },
+  theyTrustUs: { title: 'A Paris ils nous font confiance' },
+  analayse: { title: 'Analyse automatisé à Paris' },
+  faq: {
+    title1: 'FAQ Paris',
+    q1: { question: 'Paris question 17', answer: 'Paris réponse 1' },
+    q2: { question: 'Paris question 2', answer: 'Paris réponse 2' },
+    q3: { question: 'Paris question 3', answer: 'Paris réponse 3' },
+    q4: { question: 'Paris question 4', answer: 'Paris réponse 4' },
+    q5: { question: 'Paris question 5', answer: 'Paris réponse 5' },
+    q6: { question: 'Paris question 6', answer: 'Paris réponse 6' },
+    title2: 'Vous êtes particulier à Paris ?',
+  },
+  roofDiagnostics: { title: 'Diagnostique Paris title', description1: 'Description Paris 1', description2: 'Description Paris 2' },
+  footer: {
+    title: 'Votre analyse Paris',
+    description: "Votre description d'analyse Paris",
+    link: [
+      { name: 'paris', route: '/paris' },
+      { name: 'Lille', route: '/lille' },
+      { name: 'Lyon', route: '/Lyon' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+    ],
+  },
+};
