b2e45248eb3f46653af11aa31dfacfdbce68bbaf
AmourRamanantsiresy <hei.amour.2@gmail.com>
2025-07-10 13:10:24 +0000

    Add page /guerande with JSON content

---
+++ src/pages/template/json-data/guerande.ts
@@ -0,0 +1,62 @@
+export default {
+  route: 'paris',
+  hero: {
+    title: 'BIRDIA Guérande, le seul outil de diagnostique toiture',
+    items: ['Pas de CB requise pour Guérande', 'Essaie gratuit de 14 jours Guérande', "Jusqu'à 38 analyses Guérande"],
+    image: 'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/bc1cfa9f-5780-4141-a9a9-4c89cb3f8b5c',
+  },
+  testimonial: { title1: 'Témoignages clients Guérande', title2: "Passez à l'analyse intelligente à Guérande" },
+  testimonials: [
+    { name: 'Laurent M Guérande 1', star: 4, content: "Depuis petit j'adore Birdia Guérande 1", job: 'Peintre Guérande 1' },
+    { star: 2, name: 'M Laurent Guérande', content: "Depuis petit j'adore Birdia Guérande", job: 'Peintre Guérande' },
+  ],
+  analyse: {
+    information: [
+      { title: 'Guérande', description: 'Guérande description' },
+      { title: 'Guérande 1', description: 'Guérande description 1' },
+      { title: 'Guérande 2 ', description: 'Guérande description 2' },
+    ],
+    explication: ' Description analyse automatisé à Guérande',
+    image: 'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/72271dc4-0359-4281-8371-b4c425055b80',
+  },
+  theyTrustUs: {
+    title: 'A Guérande ils nous font confiance',
+    image: [
+      null,
+      'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/22eb6cf8-bf87-40ec-874e-aff1fb71e3ab',
+      'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/d9b6b070-9a32-4b58-b1b5-144600c85a48',
+    ],
+  },
+  analayse: { title: 'Analyse automatisé à Guérande' },
+  faq: {
+    title1: 'FAQ Guérande',
+    q1: { question: 'Guérande question 17', answer: 'Guérande réponse 1' },
+    q2: { question: 'Paris question 2', answer: 'Paris réponse 2' },
+    q3: { question: 'Paris question 3', answer: 'Paris réponse 3' },
+    q4: { question: 'Paris question 4', answer: 'Paris réponse 4' },
+    q5: { question: 'Paris question 5', answer: 'Paris réponse 5' },
+    q6: { question: 'Paris question 6', answer: 'Paris réponse 6' },
+    title2: 'Vous êtes particulier à Guérande ?',
+  },
+  roofDiagnostics: {
+    title: 'Diagnostique Guérande title',
+    description1: 'Description Guérande 1',
+    description2: 'Description Guérande 2',
+    image: 'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/76bfbccb-3096-4c49-b8d7-e3e1675774a9',
+  },
+  footer: {
+    title: 'Votre analyse Guérande',
+    description: "Votre description d'analyse Guérande",
+    link: [
+      { name: 'paris', route: '/paris' },
+      { name: 'Lille', route: '/lille' },
+      { name: 'Lyon', route: '/Lyon' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+    ],
+  },
+};
