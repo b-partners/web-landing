5da251407c099ac8eaa7d1e0b17ef92474309736
AmourRamanantsiresy <hei.amour.2@gmail.com>
2025-07-10 13:30:10 +0000

    Add page /caen with JSON content

---
+++ src/pages/template/json-data/caen.ts
@@ -0,0 +1,63 @@
+export default {
+  route: 'paris',
+  hero: {
+    title: 'BIRDIA Caen, le seul outil de diagnostique toiture',
+    items: ['Pas de CB requise pour Caen', 'Essaie gratuit de 14 jours Caen', "Jusqu'à 38 analyses Caen"],
+    image: 'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/8a633365-424a-45f6-9865-7f1645475cd8',
+  },
+  testimonial: { title1: 'Témoignages clients Caen', title2: "Passez à l'analyse intelligente à Caen " },
+  testimonials: [
+    { name: 'Laurent  Caen 1', star: 4, content: "Depuis petit j'adore Birdia Caen 1", job: 'Peintre Caen' },
+    { star: 2, name: 'M Laurent Caen', content: "Depuis petit j'adore Birdia Caen", job: 'Peintre Caen' },
+  ],
+  analyse: {
+    information: [
+      { title: 'Caen ', description: 'Caen description' },
+      { title: 'Caen 1', description: 'Caen 1 description' },
+      { title: 'Caen 2', description: 'Caen 2 description' },
+    ],
+    explication: ' Description analyse automatisé à Caen',
+    image: 'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/4d937bc9-c194-4ba0-a5f8-c40e84087333',
+  },
+  theyTrustUs: {
+    title: 'A Caen ils nous font confiance',
+    image: [
+      'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/24d47ee9-5563-4042-8e45-c523aa257b6b',
+      'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/a499a393-f7f4-445a-ad3c-a340cc3ce296',
+      'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/915d811e-5b25-4a54-b877-e5500bf954f4',
+      'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/fac626b9-3c74-4cb3-b4e7-c1dc2a30ab14',
+    ],
+  },
+  analayse: { title: 'Analyse automatisé à Caen' },
+  faq: {
+    title1: 'FAQ Caen',
+    q1: { question: 'Caen question 17', answer: 'Caen réponse 1' },
+    q2: { question: 'Paris question 2', answer: 'Paris réponse 2' },
+    q3: { question: 'Paris question 3', answer: 'Paris réponse 3' },
+    q4: { question: 'Paris question 4', answer: 'Paris réponse 4' },
+    q5: { question: 'Paris question 5', answer: 'Paris réponse 5' },
+    q6: { question: 'Paris question 6', answer: 'Paris réponse 6' },
+    title2: 'Vous êtes particulier à Caen ?',
+  },
+  roofDiagnostics: {
+    title: 'Diagnostique Caen title',
+    description1: 'Description Caen 1',
+    description2: 'Description Caen 2',
+    image: 'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/deb00179-880f-4702-8cd5-4f2fca3b3dcf',
+  },
+  footer: {
+    title: 'Votre analyse Caen',
+    description: "Votre description d'analyse Caen",
+    link: [
+      { name: 'paris', route: '/paris' },
+      { name: 'Lille', route: '/lille' },
+      { name: 'Lyon', route: '/Lyon' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+    ],
+  },
+};
