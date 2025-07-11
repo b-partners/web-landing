3583915413cb3273e07c806a9f507505f26fc0b9
Swonath <lanathaud@gmail.com>
2025-07-11 10:27:08 +0200

    fix: change bgcolor analyse.tsx

--- src/pages/home/components/analyse.tsx
+++ src/pages/home/components/analyse.tsx
@@ -40,7 +40,7 @@ export const Analayse: FC<AnalayseProps> = (props) => {
       <Box sx={{ flex: 1 }}>
         {((analayseData?.information || ANALYSES) as typeof ANALYSES).map((analyse, index) => {
           return isEditMode ? (
-            <Box key={index} sx={{ px: 3, py: 5, bgcolor: analyse.bgcolor }}>
+            <Box key={index} sx={{ px: 3, py: 5, bgcolor: ANALYSES[index].bgcolor, color: ANALYSES[index].color }}>
               <GenInput
                 name={`analyse.information.${index}.title`}
                 placeholder="Titre de l'analyse"
