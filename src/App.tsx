c404ba5e8523f3836cf2e1d19918a692e0db68c3
AmourRamanantsiresy <hei.amour.2@gmail.com>
2025-07-11 09:05:58 +0000

    Add page /rennes2 with JSON content

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ import { Navbar } from './common/components/navbar';
 import { Env } from './common/utils/env';
 import { PALETTE_COLORS } from './config/theme';
 import paris from './pages/template/json-data/paris';
+import rennes2 from './pages/template/json-data/rennes2';
 
 const PublicLayout = () => {
   const { pathname } = useLocation();
@@ -94,6 +95,7 @@ function App() {
           <Route path="/templateGenerator" element={<Template jsonData={paris} />} />
           <Route path="/paris" element={<Template jsonData={paris} />} />
           {/* ======== NEW TEMPLATE ROUTE REF ======== */}
+          <Route path="/rennes2" element={<Template jsonData={rennes2} />} />
           {/* Do not remote this two comments, they are use full for template page generation */}
         </Route>
       </Routes>
---
+++ src/pages/template/json-data/rennes2.ts
@@ -0,0 +1,103 @@
+export default {
+  route: 'paris',
+  hero: {
+    title: 'BIRDIA, Rennes : votre outil intelligent pour diagnostiquer les toitures',
+    items: ['Pas besoin de se déplacer à Rennes', '14 jours d’essai gratuit', 'Jusqu’à 38 diagnostics de toitures inclus'],
+    image: 'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/4b3e3bb3-33c7-4f7c-9c3a-0299c56ce508',
+  },
+  testimonial: { title1: 'Témoignages clients Rennes :', title2: 'Passez à l’analyse intelligente de vos toitures à Rennes, sans prise de tête.' },
+  testimonials: [
+    {
+      name: 'Idris B.',
+      star: 2,
+      content:
+        '« Je ne perds plus de temps à sillonner Rennes. Birdia me permet de faire mes diagnostics sans bouger de mon bureau. Un vrai gain de temps pour mon activité. »',
+      job: 'Couvreur à Rennes',
+    },
+    {
+      star: 3,
+      name: 'Arnaud P.',
+      content: '« Birdia est rapide et simple. Je fais mes devis en un clic avant même de visiter le chantier. C’est un atout énorme dans mon quotidien. »',
+      job: "Chef d'entreprise en couverture à Cesson-Sévigné",
+    },
+    {
+      name: 'Laurent M.',
+      star: 5,
+      content:
+        '"Birdia est un véritable game changer. L’outil est à la fois intuitif, puissant et ludique. En quelques clics, nous pouvons visualiser, détecter et évaluer des centaines de toitures. Cela change notre manière de penser la prévention et le service client."',
+      job: 'Responsable Innovation – Assureur Habitation',
+    },
+    {
+      star: 5,
+      name: 'Émilie C.',
+      content:
+        '"Grâce à Birdia, nos équipes disposent d’une vision détaillée de chaque bien assuré. La précision de l’analyse des matériaux, de l’usure et des risques nous permet d’adapter finement les contrats. C’est un vrai plus pour la maîtrise technique et commerciale."',
+      job: 'Responsable Souscription Habitation – Groupe IARD',
+    },
+  ],
+  analyse: {
+    title: 'Analyse automatisée de toitures par intelligence artificielle',
+    information: [
+      {
+        title: 'Analyse et rapports en temps réel',
+        description: 'Obtenez en quelques secondes un rapport clair sur les toitures rennaises pour mieux planifier vos travaux.',
+      },
+      {
+        title: 'Comment fonctionne notre technologie ?',
+        description:
+          'Avec une simple adresse à Rennes ou en périphérie, notre outil analyse automatiquement les toitures pour vous fournir un diagnostic fiable.',
+      },
+      {
+        title: 'Un traitement d’image avancé',
+        description:
+          'Notre intelligence artificielle transforme des photos aériennes très précises en informations faciles à comprendre, sans intervention humaine.',
+      },
+    ],
+    explication:
+      'Grâce à des images prises depuis le ciel, notre outil identifie les défauts, classe les priorités et vous fait gagner un temps précieux, sans monter sur le toit.',
+    image: 'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/b795c727-2562-4c26-8cff-8d26dd0976cc',
+  },
+  theyTrustUs: {
+    title: 'Ils nous font confiance à Rennes et ailleurs :',
+    image: [
+      'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/49bf5c62-06e5-4db2-a9ba-3bf170be6048',
+      'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/e2a4e92b-fb12-463d-960a-d0bc3acd74cb',
+      'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/d992d5bf-ed94-40c3-9481-b3ed0bd79bfe',
+      'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/5a6211d5-aafc-448a-b764-2d9e16467fae',
+    ],
+  },
+  faq: {
+    title1: 'FAQ Rennes :',
+    q1: { question: 'À quoi sert l’analyse d’un toit à Rennes ?', answer: ' Identifier les problèmes comme les fuites ou les isolations défectueuses.' },
+    q2: { question: 'Est-ce un bon investissement ?', answer: 'Oui, cela évite des travaux coûteux non anticipés.' },
+    q3: { question: 'Quelles différences avec d’autres outils ?', answer: 'Birdia ne demande ni visite sur place, ni intervention physique.' },
+    q4: { question: 'Question 4', answer: 'Réponse 4' },
+    q5: { question: 'Question 5', answer: 'Réponse 5' },
+    q6: { question: 'Question 6', answer: 'Réponse 6' },
+    title2: 'Vous êtes un particulier à Rennes , vous souhaitez trouver un couvreur ?',
+  },
+  roofDiagnostics: {
+    title: 'Diagnostic toiture à Rennes',
+    description1:
+      'Chez Birdia.fr, nous réalisons un diagnostic toiture complet pour les bâtiments de Rennes et sa métropole. On repère les fuites, les zones abîmées et les problèmes d’isolation, le tout à partir de vues aériennes.',
+    description2:
+      'Nos experts vous remettent un rapport détaillé, pour anticiper les travaux et prolonger la durée de vie de votre toiture. Que vous soyez propriétaire ou futur acheteur, c’est un service rassurant qui évite les mauvaises surprises. Grâce à notre technologie de pointe, on détecte même ce que l’œil ne voit pas !',
+    image: 'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/7e4df089-74b2-4085-9bc1-2e4e79430c87',
+  },
+  footer: {
+    title: 'Votre analyse Birdia à Rennes',
+    description: 'Découvrez nos services dans toute la métropole rennaise :',
+    link: [
+      { name: 'Birdia Paris', route: 'https://fr.wikipedia.org/wiki/Paris' },
+      { name: 'Birdia Cesson ', route: 'https://fr.wikipedia.org/wiki/Cesson' },
+      { name: 'Birdia Toulouse ', route: 'https://fr.wikipedia.org/wiki/Toulouse' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+    ],
+  },
+  analayse: { title: 'Analyse automatisée de toitures par intelligence artificielle' },
+};
