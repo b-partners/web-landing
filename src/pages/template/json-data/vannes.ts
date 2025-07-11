7c0f5b4bfc2cb920549ea130dbcf19f4ebeca752
AmourRamanantsiresy <hei.amour.2@gmail.com>
2025-07-11 12:16:16 +0000

    Add page /vannes with JSON content

---
+++ src/pages/template/json-data/vannes.ts
@@ -0,0 +1,98 @@
+export default {
+  route: 'paris',
+  hero: {
+    title: 'BIRDIA Poitier, le seul outil de diagnostique toiture',
+    items: ['Pas de CB requise Poitier', 'Essaie gratuit de 14 jours Poitier', "Jusqu'à 38 analyses Poitier"],
+    image: 'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/8fc75b84-2d40-4e5d-87ed-a3c4dde3bcc0',
+  },
+  testimonial: { title1: 'Témoignages clients Poitier', title2: 'Passez à l’analyse intelligente, sans compléxité Poitier' },
+  testimonials: [
+    {
+      name: 'Arnaud P. Poitier',
+      star: 5,
+      content:
+        'L’outil Birdia est simple, rapide et fiable. Je peux générer un rapport technique avant même de visiter le chantier. Cela me fait gagner des heures chaque semaine, tout en améliorant la qualité de mes prestations. Poitier',
+      job: 'Gérant entreprise de couverture (Cannes) Poitier',
+    },
+    {
+      star: 5,
+      name: 'Joël D. Poitier',
+      content:
+        'Birdia, c’est mon assistant digital. Je l’utilise au quotidien pour détecter les toits à rénover, et les clients adorent recevoir un rapport visuel clair. Même mes devis sont mieux acceptés grâce à ça. Poitier',
+      job: 'Couvreur expérimenté (Hauts-de-France) Poitier',
+    },
+    {
+      name: 'Laurent M. Poitier',
+      star: 5,
+      content:
+        'Birdia est un véritable game changer. L’outil est à la fois intuitif, puissant et ludique. En quelques clics, nous pouvons visualiser, détecter et évaluer des centaines de toitures. Cela change notre manière de penser la prévention et le service client. Poitier',
+      job: 'Responsable Innovation – Assureur Habitation Poitier',
+    },
+    {
+      star: 5,
+      name: 'Émilie C. Poitier',
+      content:
+        'Grâce à Birdia, nos équipes disposent d’une vision détaillée de chaque bien assuré. La précision de l’analyse des matériaux, de l’usure et des risques nous permet d’adapter finement les contrats. C’est un vrai plus pour la maîtrise technique et commerciale. Poitier',
+      job: 'Responsable Souscription Habitation – Groupe IARD Poitier',
+    },
+  ],
+  analyse: {
+    title: 'Analyse automatisée de toitures par intelligence artificielle Poitier',
+    information: [
+      { title: 'Analyse et rapports en temps réel Poitier', description: 'Analyse en temps réel pour prioriser vos interventions et gagner du temps. Poitier' },
+      {
+        title: 'Comment fonctionne notre technologie ? Poitier',
+        description: 'Avec une simple adresse postale, analysez automatiquement les toitures de vos clients et prospects. Poitier',
+      },
+      {
+        title: 'Un traitement d’image avancé Poitier',
+        description: 'Notre IA transforme des images HD 5cm de résolution en données claires et exploitables, sans saisie manuelle. Poitier',
+      },
+    ],
+    explication:
+      ' Détection, qualification et recommandation à partir d’images aériennes HD. En un clic, obtenez un diagnostic métier précis sans monter sur le toit. Poitier',
+    image: 'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/36cfee90-71f0-4ba1-96c9-dba05cb11e7b',
+  },
+  theyTrustUs: {
+    title: 'Ils nous font confiance Poitier',
+    image: [
+      null,
+      'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/4c92847d-64db-4e36-b239-5e46b3eb2200',
+      'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/3bf07834-a15f-43fe-aac4-e651986df9ab',
+      'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/cec55e21-fa4f-4090-9585-73c2a5d7ba3c',
+    ],
+  },
+  faq: {
+    title1: 'FAQ Poitier',
+    q1: { question: 'Question 1 Poitier', answer: 'Réponse 1 Poitier' },
+    q2: { question: 'Question 2 Poitier', answer: 'Réponse 2 Poitier' },
+    q3: { question: 'Question 3 Poitier', answer: 'Réponse 3 Poitier' },
+    q4: { question: 'Question 4 Poitier', answer: 'Réponse 4 Poitier' },
+    q5: { question: 'Question 5 Poitier', answer: 'Réponse 5 Poitier' },
+    q6: { question: 'Question 6 Poitier', answer: 'Réponse 6 Poitier' },
+    title2: 'Vous êtes un particulier , vous souhaitez trouver un couvreur ? Poitier',
+  },
+  roofDiagnostics: {
+    title: 'Diagnostique toiture Poitier',
+    description1:
+      "Chez Birdia.fr, nous vous proposons un diagnostic toiture complet pour évaluer l’état de votre couverture, repérer les éventuelles infiltrations, dégradations ou défauts d’isolation. Ce service essentiel vous permet d’anticiper les travaux, d'éviter les mauvaises surprises et de prolonger la durée de vie de votre toiture. Nos experts interviennent rapidement, avec un rapport détaillé et des recommandations claires. Poitier",
+    description2:
+      'Que vous soyez propriétaire ou futur acquéreur, un diagnostic toiture réalisé par des professionnels qualifiés vous assure une tranquillité d’esprit. Grâce à notre expertise et à des outils de pointe, nous détectons les moindres anomalies, visibles ou invisibles à l’œil nu. Poitier',
+    image: 'https://puw7vdgvdl.execute-api.eu-west-3.amazonaws.com/Prod/landing-file/9e413c8e-249d-45d7-8761-4148b984404d',
+  },
+  footer: {
+    title: 'Votre analyse Birdia près de chez vous Poitier',
+    description: 'Découvrez nos offres disponibles dans les villes suivantes : Poitier',
+    link: [
+      { name: 'Poitier', route: 'https://fr.wikipedia.org/wiki/Paris' },
+      { name: 'Poitier', route: 'https://fr.wikipedia.org/wiki/Paris' },
+      { name: 'Poitier', route: 'https://fr.wikipedia.org/wiki/Paris' },
+      { name: 'Poitier', route: 'https://fr.wikipedia.org/wiki/Paris' },
+      { name: 'Poitier', route: 'https://fr.wikipedia.org/wiki/Paris' },
+      { name: 'Poitier', route: 'https://fr.wikipedia.org/wiki/Paris' },
+      { name: 'Poitier', route: 'https://fr.wikipedia.org/wiki/Paris' },
+      { name: 'Poitier', route: 'https://fr.wikipedia.org/wiki/Paris' },
+      { name: 'Poitier', route: 'https://fr.wikipedia.org/wiki/Paris' },
+    ],
+  },
+};
