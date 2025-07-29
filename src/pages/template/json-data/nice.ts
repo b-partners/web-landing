b5743c8f450eb2a93ed7ad30adfbf6e5c23b3344
AmourRamanantsiresy <hei.amour.2@gmail.com>
2025-07-29 13:56:09 +0000

    Add page /nice with JSON content

---
+++ src/pages/template/json-data/nice.ts
@@ -0,0 +1,86 @@
+export default {
+  route: 'paris',
+  metaTitle: 'BIRDIA, l’IA qui analyse vos toitures sur images HD',
+  metaDescription: "Mesures précises, détection d'anomalies (usures, moisissures...), et gestion de dossiers. Gagnez du temps, optimisez vos interventions !",
+  hero: { title: 'BIRDIA, le seul outil de diagnostique toiture', items: ['Pas de CB requise', 'Essaie gratuit de 14 jours', "Jusqu'à 38 analyses"] },
+  testimonial: { title1: 'Témoignages clients', title2: 'Passez à l’analyse intelligente, sans compléxité' },
+  testimonials: [
+    {
+      name: 'Arnaud P.',
+      star: 5,
+      content:
+        'L’outil Birdia est simple, rapide et fiable. Je peux générer un rapport technique avant même de visiter le chantier. Cela me fait gagner des heures chaque semaine, tout en améliorant la qualité de mes prestations.',
+      job: 'Gérant entreprise de couverture (Cannes)',
+    },
+    {
+      star: 5,
+      name: 'Joël D.',
+      content:
+        'Birdia, c’est mon assistant digital. Je l’utilise au quotidien pour détecter les toits à rénover, et les clients adorent recevoir un rapport visuel clair. Même mes devis sont mieux acceptés grâce à ça.',
+      job: 'Couvreur expérimenté (Hauts-de-France)',
+    },
+    {
+      name: 'Laurent M.',
+      star: 5,
+      content:
+        'Birdia est un véritable game changer. L’outil est à la fois intuitif, puissant et ludique. En quelques clics, nous pouvons visualiser, détecter et évaluer des centaines de toitures. Cela change notre manière de penser la prévention et le service client.',
+      job: 'Responsable Innovation – Assureur Habitation',
+    },
+    {
+      star: 5,
+      name: 'Émilie C.',
+      content:
+        'Grâce à Birdia, nos équipes disposent d’une vision détaillée de chaque bien assuré. La précision de l’analyse des matériaux, de l’usure et des risques nous permet d’adapter finement les contrats. C’est un vrai plus pour la maîtrise technique et commerciale.',
+      job: 'Responsable Souscription Habitation – Groupe IARD',
+    },
+  ],
+  analyse: {
+    title: 'Analyse automatisée de toitures par intelligence artificielle',
+    information: [
+      { title: 'Analyse et rapports en temps réel', description: 'Analyse en temps réel pour prioriser vos interventions et gagner du temps.' },
+      {
+        title: 'Comment fonctionne notre technologie ?',
+        description: 'Avec une simple adresse postale, analysez automatiquement les toitures de vos clients et prospects.',
+      },
+      {
+        title: 'Un traitement d’image avancé',
+        description: 'Notre IA transforme des images HD 5cm de résolution en données claires et exploitables, sans saisie manuelle.',
+      },
+    ],
+    explication:
+      ' Détection, qualification et recommandation à partir d’images aériennes HD. En un clic, obtenez un diagnostic métier précis sans monter sur le toit.',
+  },
+  theyTrustUs: { title: 'Ils nous font confiance' },
+  faq: {
+    title1: 'FAQ',
+    q1: { question: 'Question 1', answer: 'Réponse 1' },
+    q2: { question: 'Question 2', answer: 'Réponse 2' },
+    q3: { question: 'Question 3', answer: 'Réponse 3' },
+    q4: { question: 'Question 4', answer: 'Réponse 4' },
+    q5: { question: 'Question 5', answer: 'Réponse 5' },
+    q6: { question: 'Question 6', answer: 'Réponse 6' },
+    title2: 'Vous êtes un particulier , vous souhaitez trouver un couvreur ?',
+  },
+  roofDiagnostics: {
+    title: 'Diagnostique toiture',
+    description1:
+      "Chez Birdia.fr, nous vous proposons un diagnostic toiture complet pour évaluer l’état de votre couverture, repérer les éventuelles infiltrations, dégradations ou défauts d’isolation. Ce service essentiel vous permet d’anticiper les travaux, d'éviter les mauvaises surprises et de prolonger la durée de vie de votre toiture. Nos experts interviennent rapidement, avec un rapport détaillé et des recommandations claires.",
+    description2:
+      'Que vous soyez propriétaire ou futur acquéreur, un diagnostic toiture réalisé par des professionnels qualifiés vous assure une tranquillité d’esprit. Grâce à notre expertise et à des outils de pointe, nous détectons les moindres anomalies, visibles ou invisibles à l’œil nu.',
+  },
+  footer: {
+    title: 'Votre analyse Birdia près de chez vous',
+    description: 'Découvrez nos offres disponibles dans les villes suivantes :',
+    link: [
+      { name: 'paris', route: '/paris' },
+      { name: 'Lille', route: '/lille' },
+      { name: 'Lyon', route: '/Lyon' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+    ],
+  },
+};
