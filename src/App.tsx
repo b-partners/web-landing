80a20aee44f7942d8b496db7b1fabfc8d07248fe
AmourRamanantsiresy <hei.amour.2@gmail.com>
2025-08-08 12:43:58 +0000

    Add page /diagnostic-toiture-achat-maison with JSON content

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import { useSnackbar } from '@store/snackbar';
 import { Navbar } from './common/components/navbar';
 import { Env } from './common/utils/env';
 import { PALETTE_COLORS } from './config/theme';
+import diagnosticToitureAchatMaison from './pages/template/json-data/diagnostic-toiture-achat-maison';
 import paris from './pages/template/json-data/paris';
 
 const PublicLayout = () => {
@@ -95,6 +96,7 @@ function App() {
           <Route path="/templateGenerator" element={<Template jsonData={paris} />} />
           <Route path="/paris" element={<Template jsonData={paris} />} />
           {/* ======== NEW TEMPLATE ROUTE REF ======== */}
+          <Route path="/diagnostic-toiture-achat-maison" element={<Template jsonData={diagnosticToitureAchatMaison} />} />
           {/* Do not remote this two comments, they are use full for template page generation */}
         </Route>
       </Routes>
