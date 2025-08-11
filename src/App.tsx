3564836d67f83ee4b35dbb234efea5e6e51065c8
Sophie-94 <dasilvapereira@live.fr>
2025-08-11 19:14:57 +0200

    chore: Add page /expert-en-toiture with JSON content

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import { useSnackbar } from '@store/snackbar';
 import { Navbar } from './common/components/navbar';
 import { Env } from './common/utils/env';
 import { PALETTE_COLORS } from './config/theme';
+import expertEnToiture from './pages/template/json-data/expert-en-toiture';
 import expertiseToiture from './pages/template/json-data/expertise-toiture';
 import expertToiture from './pages/template/json-data/expert-toiture';
 import evry from './pages/template/json-data/evry';
@@ -102,6 +103,7 @@ function App() {
           <Route path="/templateGenerator" element={<Template jsonData={paris} />} />
           <Route path="/paris" element={<Template jsonData={paris} />} />
           {/* ======== NEW TEMPLATE ROUTE REF ======== */}
+          <Route path="/expert-en-toiture" element={<Template jsonData={expertEnToiture} />} />
           <Route path="/expertise-toiture" element={<Template jsonData={expertiseToiture} />} />
           <Route path="/expert-toiture" element={<Template jsonData={expertToiture} />} />
           <Route path="/evry" element={<Template jsonData={evry} />} />
