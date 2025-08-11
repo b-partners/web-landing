a88a44b8621b084df85c3103cbc9834834ff26e4
AmourRamanantsiresy <hei.amour.2@gmail.com>
2025-08-11 13:49:01 +0000

    Add page /diagnostic-toiture-100m2 with JSON content

--- src/App.tsx
+++ src/App.tsx
@@ -18,11 +18,12 @@ import { useSnackbar } from '@store/snackbar';
 import { Navbar } from './common/components/navbar';
 import { Env } from './common/utils/env';
 import { PALETTE_COLORS } from './config/theme';
-import evry from './pages/template/json-data/evry';
-import essonne from './pages/template/json-data/essonne';
 import diagnisticToitureEssonne from './pages/template/json-data/diagnistic-toiture-essonne';
-import landingPage from './pages/template/json-data/landing-page';
 import diagnosticToiture from './pages/template/json-data/diagnostic-toiture';
+import diagnosticToiture100m2 from './pages/template/json-data/diagnostic-toiture-100m2';
+import essonne from './pages/template/json-data/essonne';
+import evry from './pages/template/json-data/evry';
+import landingPage from './pages/template/json-data/landing-page';
 import paris from './pages/template/json-data/paris';
 
 const PublicLayout = () => {
@@ -100,6 +101,7 @@ function App() {
           <Route path="/templateGenerator" element={<Template jsonData={paris} />} />
           <Route path="/paris" element={<Template jsonData={paris} />} />
           {/* ======== NEW TEMPLATE ROUTE REF ======== */}
+          <Route path="/diagnostic-toiture-100m2" element={<Template jsonData={diagnosticToiture100m2} />} />
           <Route path="/evry" element={<Template jsonData={evry} />} />
           <Route path="/essonne" element={<Template jsonData={essonne} />} />
           <Route path="/diagnistic-toiture-essonne" element={<Template jsonData={diagnisticToitureEssonne} />} />
