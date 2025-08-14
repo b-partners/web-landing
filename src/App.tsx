4e5931d83e5a0e19c188fe967aa34b8a127e8fb2
AmourRamanantsiresy <hei.amour.2@gmail.com>
2025-08-14 10:27:34 +0000

    Add page /etat-toiture-avant-vente with JSON content

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import diagnisticToitureEssonne from './pages/template/json-data/diagnistic-toit
 import diagnosticToit from './pages/template/json-data/diagnostic-toit';
 import diagnosticToiture from './pages/template/json-data/diagnostic-toiture';
 import essonne from './pages/template/json-data/essonne';
+import etatToitureAvantVente from './pages/template/json-data/etat-toiture-avant-vente';
 import evry from './pages/template/json-data/evry';
 import expertEnToiture from './pages/template/json-data/expert-en-toiture';
 import expertToiture from './pages/template/json-data/expert-toiture';
@@ -113,6 +114,7 @@ function App() {
           <Route path="/templateGenerator" element={<Template jsonData={paris} />} />
           <Route path="/paris" element={<Template jsonData={paris} />} />
           {/* ======== NEW TEMPLATE ROUTE REF ======== */}
+          <Route path="/etat-toiture-avant-vente" element={<Template jsonData={etatToitureAvantVente} />} />
           <Route path="/schéma-toiture" element={<Template jsonData={schMaToiture} />} />
           <Route path="/refection-toitures" element={<Template jsonData={refectionToitures} />} />
           <Route path="/refection-toiture-definition" element={<Template jsonData={refectionToitureDefinition} />} />
