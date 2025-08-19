4b2bddb950d5c6ec60c741f602e34d9c73bc36ba
Coinaute-redaction <elise@coinaute.com>
2025-08-19 22:09:00 +0200

    chore: Add page /infiltration-eau-toiture with JSON content

--- src/App.tsx
+++ src/App.tsx
@@ -70,6 +70,7 @@ import evry from './pages/template/json-data/evry';
 import expertEnToiture from './pages/template/json-data/expert-en-toiture';
 import expertToiture from './pages/template/json-data/expert-toiture';
 import expertiseToiture from './pages/template/json-data/expertise-toiture';
+import infiltrationEauToiture from './pages/template/json-data/infiltration-eau-toiture';
 import infiltrationEauToitureTerrasse from './pages/template/json-data/infiltration-eau-toiture-terrasse';
 import immobilierToiture from './pages/template/json-data/immobilier-toiture';
 import gratuitEtatToitureImmeuble from './pages/template/json-data/gratuit-etat-toiture-immeuble';
