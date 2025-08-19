37f34c4dce91c59614df957b6cd925398d4d5d10
Coinaute-redaction <elise@coinaute.com>
2025-08-19 21:56:00 +0200

    chore: Add page /etat-toiture-avant-achat with JSON content

--- src/App.tsx
+++ src/App.tsx
@@ -51,6 +51,7 @@ import diagnosticToitureDrone from './pages/template/json-data/diagnostic-toitur
 import diagnosticToitureZinc from './pages/template/json-data/diagnostic-toiture-zinc';
 import dianosticAvantVente from './pages/template/json-data/dianostic-avant-vente';
 import essonne from './pages/template/json-data/essonne';
+import etatToitureAvantAchat from './pages/template/json-data/etat-toiture-avant-achat';
 import etancheiteToitureMetallique from './pages/template/json-data/etancheite-toiture-metallique';
 import evry from './pages/template/json-data/evry';
 import expertEnToiture from './pages/template/json-data/expert-en-toiture';
@@ -153,6 +154,7 @@ function App() {
           <Route path="/templateGenerator" element={<Template jsonData={paris} />} />
           <Route path="/paris" element={<Template jsonData={paris} />} />
           {/* ======== NEW TEMPLATE ROUTE REF ======== */}
+          <Route path="/etat-toiture-avant-achat" element={<Template jsonData={etatToitureAvantAchat} />} />
           <Route path="/diagnostic-etat-toiture" element={<Template jsonData={diagnosticEtatToiture} />} />
           <Route path="/etancheite-toiture-metallique" element={<Template jsonData={etancheiteToitureMetallique} />} />
           <Route path="/couvreur-renovation-toiture" element={<Template jsonData={couvreurRenovationToiture} />} />
