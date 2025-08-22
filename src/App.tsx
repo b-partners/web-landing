25eee7ab24c614886123585c584619fe52abf8cd
Coinaute-redaction <elise@coinaute.com>
2025-08-22 08:24:51 +0200

    Add page /diagnostic-technique-toiture with JSON content
    Co-authored-by: AmourRamanantsiresy <hei.amour.2@gmail.com>
    Co-authored-by: Ricka Princy <127594276+RickaPrincy@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -49,6 +49,7 @@ import diagnosticGratuitToitureImmeuble from './pages/template/json-data/diagnos
 import diagnosticImmobilierToitures from './pages/template/json-data/diagnostic-immobilier-toitures';
 import diagnosticIndependantDeToiture from './pages/template/json-data/diagnostic-independant-de-toiture';
 import diagnosticIndependantToiture from './pages/template/json-data/diagnostic-independant-toiture';
+import diagnosticTechniqueToiture from './pages/template/json-data/diagnostic-technique-toiture';
 import diagnosticRhone from './pages/template/json-data/diagnostic-rhone';
 import diagnosticToit from './pages/template/json-data/diagnostic-toit';
 import diagnosticToiture from './pages/template/json-data/diagnostic-toiture';
@@ -195,6 +196,7 @@ function App() {
           <Route path="/templateGenerator" element={<Template jsonData={paris} />} />
           <Route path="/paris" element={<Template jsonData={paris} />} />
           {/* ======== NEW TEMPLATE ROUTE REF ======== */}
+          <Route path="/diagnostic-technique-toiture" element={<Template jsonData={diagnosticTechniqueToiture} />} />
           <Route path="/diagnostic-rhone" element={<Template jsonData={diagnosticRhone} />} />
           <Route path="/performance-toiture" element={<Template jsonData={performanceToiture} />} />
           <Route path="/isolation-toiture" element={<Template jsonData={isolationToiture} />} />
