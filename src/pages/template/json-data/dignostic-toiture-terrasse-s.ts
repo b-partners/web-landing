f98c17b32cb0a112013482e69e86d539e4f8a691
Sophie-94 <dasilvapereira@live.fr>
2025-09-02 13:58:53 +0200

    chore: Add page /dignostic-toiture-terrasse-s with JSON content

---
+++ src/pages/template/json-data/dignostic-toiture-terrasse-s.ts
@@ -0,0 +1,97 @@
+export default {
+  route: 'paris',
+  metaTitle: 'Diagnostic toiture-terrasse : une vérification indispensable pour prévenir les infiltrations',
+  metaDescription:
+    'Découvrez pourquoi réaliser un diagnostic toiture-terrasse est essentiel, ses étapes clés et ses bénéfices pour assurer l’étanchéité et la durabilité de votre bâtiment.',
+  hero: {
+    title: 'Diagnostic toiture-terrasse : une vérification indispensable pour prévenir les infiltrations',
+    items: [
+      'Un diagnostic toiture-terrasse contrôle l’état de l’étanchéité, de l’isolation et du support.',
+      'Il permet de détecter fissures, stagnations d’eau et défauts de conception.',
+      'Recommandé avant travaux, après intempéries ou dans le cadre d’une vente immobilière.',
+    ],
+  },
+  testimonial: { title1: 'Témoignages clients', title2: 'Passez à l’analyse intelligente, sans compléxité' },
+  testimonials: [
+    {
+      name: 'Arnaud P.',
+      star: 5,
+      content:
+        '"L’outil Birdia est simple, rapide et fiable. Je peux générer un rapport technique avant même de visiter le chantier. Cela me fait gagner des heures chaque semaine, tout en améliorant la qualité de mes prestations."',
+      job: 'Gérant entreprise de couverture (Cannes)',
+    },
+    {
+      star: 5,
+      name: 'Joël D.',
+      content:
+        'Birdia, c’est mon assistant digital. Je l’utilise au quotidien pour détecter les toits à rénover, et les clients adorent recevoir un rapport visuel clair. Même mes devis sont mieux acceptés grâce à ça.',
+      job: 'Couvreur expérimenté (Hauts-de-France)',
+    },
+    {
+      name: 'Laurent M.',
+      star: 5,
+      content:
+        'Birdia est un véritable game changer. L’outil est à la fois intuitif, puissant et ludique. En quelques clics, nous pouvons visualiser, détecter et évaluer des centaines de toitures. Cela change notre manière de penser la prévention et le service client.',
+      job: 'Responsable Innovation – Assureur Habitation',
+    },
+    {
+      star: 5,
+      name: 'Émilie C.',
+      content:
+        'Grâce à Birdia, nos équipes disposent d’une vision détaillée de chaque bien assuré. La précision de l’analyse des matériaux, de l’usure et des risques nous permet d’adapter finement les contrats. C’est un vrai plus pour la maîtrise technique et commerciale.',
+      job: 'Responsable Souscription Habitation – Groupe IARD',
+    },
+  ],
+  analyse: {
+    title: 'Analyse automatisée de toitures par intelligence artificielle',
+    information: [
+      {
+        title: 'Pourquoi diagnostic toiture-terrasse est essentiel ?',
+        description: 'Il identifie les risques d’infiltration et garantit la conformité aux règles de construction.',
+      },
+      {
+        title: 'Comment se déroule un diagnostic toiture-terrasse ?',
+        description: 'Inspection visuelle, relevés d’humidité, contrôle des joints et analyse de l’isolation.',
+      },
+      {
+        title: 'Que faire après diagnostic toiture-terrasse ?',
+        description: 'Effectuer réparations ciblées, renforcer l’étanchéité ou planifier une rénovation complète.',
+      },
+    ],
+    explication:
+      ' Détection, qualification et recommandation à partir d’images aériennes HD. En un clic, obtenez un diagnostic métier précis sans monter sur le toit.',
+  },
+  theyTrustUs: { title: 'Ils nous font confiance' },
+  faq: {
+    title1: 'FAQ',
+    q1: { question: 'Question 1', answer: 'Réponse 1' },
+    q2: { question: 'Question 2', answer: 'Réponse 2' },
+    q3: { question: 'Question 3', answer: 'Réponse 3' },
+    q4: { question: 'Question 4', answer: 'Réponse 4' },
+    q5: { question: 'Question 5', answer: 'Réponse 5' },
+    q6: { question: 'Question 6', answer: 'Réponse 6' },
+    title2: 'Vous êtes un particulier , vous souhaitez trouver un couvreur ?',
+  },
+  roofDiagnostics: {
+    title: 'Comprendre les enjeux long terme de diagnostic toiture-terrasse',
+    description1:
+      'Un diagnostic toiture-terrasse est une démarche stratégique pour maintenir la performance et la sécurité d’un bâtiment. Les toitures-terrasses sont particulièrement exposées aux variations climatiques et aux contraintes d’étanchéité. Ce contrôle permet d’anticiper les dégradations liées à l’humidité, de prolonger la durée de vie des matériaux et d’éviter des réparations lourdes et coûteuses. ',
+    description2:
+      'Sur le long terme, il contribue à préserver la structure porteuse, à améliorer le confort thermique et à protéger la valeur immobilière. Intégré dans un suivi régulier, il devient un outil essentiel de gestion et d’entretien durable du patrimoine.',
+  },
+  footer: {
+    title: 'Votre analyse Birdia près de chez vous',
+    description: 'Découvrez nos offres disponibles dans les villes suivantes :',
+    link: [
+      { name: 'paris', route: '/paris' },
+      { name: 'Lille', route: '/lille' },
+      { name: 'Lyon', route: '/Lyon' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+    ],
+  },
+};
