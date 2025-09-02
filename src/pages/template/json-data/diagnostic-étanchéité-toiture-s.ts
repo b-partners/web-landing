0cd184ac3b98b0560713413ab3852073728bbf0a
Sophie-94 <dasilvapereira@live.fr>
2025-09-02 14:01:34 +0200

    chore: Add page /diagnostic-étanchéité-toiture-s with JSON content

---
+++ src/pages/template/json-data/diagnostic-étanchéité-toiture-s.ts
@@ -0,0 +1,97 @@
+export default {
+  route: 'paris',
+  metaTitle: 'Diagnostic étanchéité toiture : prévenir infiltrations et préserver la durabilité',
+  metaDescription:
+    'Découvrez pourquoi un diagnostic étanchéité toiture est indispensable, ses étapes et ses bénéfices pour assurer la protection et la longévité de votre bâtiment.',
+  hero: {
+    title: 'Diagnostic étanchéité toiture : prévenir infiltrations et préserver la durabilité',
+    items: [
+      'Un diagnostic étanchéité toiture détecte fuites, fissures et défauts de revêtement.',
+      'Il permet d’anticiper les infiltrations et de limiter les dégâts structurels.',
+      'Recommandé en entretien préventif, avant travaux ou après des intempéries.',
+    ],
+  },
+  testimonial: { title1: 'Témoignages clients', title2: 'Passez à l’analyse intelligente, sans compléxité' },
+  testimonials: [
+    {
+      name: 'Arnaud P.',
+      star: 5,
+      content:
+        '"L’outil Birdia est simple, rapide et fiable. Je peux générer un rapport technique avant même de visiter le chantier. Cela me fait gagner des heures chaque semaine, tout en améliorant la qualité de mes prestations."',
+      job: 'Gérant entreprise de couverture (Cannes)',
+    },
+    {
+      star: 5,
+      name: 'Joël D.',
+      content:
+        'Birdia, c’est mon assistant digital. Je l’utilise au quotidien pour détecter les toits à rénover, et les clients adorent recevoir un rapport visuel clair. Même mes devis sont mieux acceptés grâce à ça.',
+      job: 'Couvreur expérimenté (Hauts-de-France)',
+    },
+    {
+      name: 'Laurent M.',
+      star: 5,
+      content:
+        'Birdia est un véritable game changer. L’outil est à la fois intuitif, puissant et ludique. En quelques clics, nous pouvons visualiser, détecter et évaluer des centaines de toitures. Cela change notre manière de penser la prévention et le service client.',
+      job: 'Responsable Innovation – Assureur Habitation',
+    },
+    {
+      star: 5,
+      name: 'Émilie C.',
+      content:
+        'Grâce à Birdia, nos équipes disposent d’une vision détaillée de chaque bien assuré. La précision de l’analyse des matériaux, de l’usure et des risques nous permet d’adapter finement les contrats. C’est un vrai plus pour la maîtrise technique et commerciale.',
+      job: 'Responsable Souscription Habitation – Groupe IARD',
+    },
+  ],
+  analyse: {
+    title: 'Analyse automatisée de toitures par intelligence artificielle',
+    information: [
+      {
+        title: 'Pourquoi diagnostic étanchéité toiture est essentiel ?',
+        description: 'Il identifie les points faibles pour éviter infiltrations et préserver la structure du bâtiment.',
+      },
+      {
+        title: "Comment se déroule un diagnostic d'étanchéité toiture ?",
+        description: 'Inspection visuelle, tests d’humidité, vérification des joints et relevés techniques précis.',
+      },
+      {
+        title: 'Que faire après diagnostic étanchéité toiture ?',
+        description: 'Programmer réparations ciblées, renforcer protections ou planifier une rénovation complète.',
+      },
+    ],
+    explication:
+      ' Détection, qualification et recommandation à partir d’images aériennes HD. En un clic, obtenez un diagnostic métier précis sans monter sur le toit.',
+  },
+  theyTrustUs: { title: 'Ils nous font confiance' },
+  faq: {
+    title1: 'FAQ',
+    q1: { question: 'Question 1', answer: 'Réponse 1' },
+    q2: { question: 'Question 2', answer: 'Réponse 2' },
+    q3: { question: 'Question 3', answer: 'Réponse 3' },
+    q4: { question: 'Question 4', answer: 'Réponse 4' },
+    q5: { question: 'Question 5', answer: 'Réponse 5' },
+    q6: { question: 'Question 6', answer: 'Réponse 6' },
+    title2: 'Vous êtes un particulier , vous souhaitez trouver un couvreur ?',
+  },
+  roofDiagnostics: {
+    title: 'Comprendre les enjeux long terme de diagnostic étanchéité toiture',
+    description1:
+      'Un diagnostic étanchéité toiture est une démarche clé pour prolonger la durée de vie d’un bâtiment. L’eau est la principale cause de dégradations, entraînant infiltrations, affaiblissement des structures et perte d’efficacité énergétique. Ce contrôle permet d’anticiper ces problèmes et de mettre en place un plan d’entretien adapté. ',
+    description2:
+      'À long terme, il contribue à préserver l’intégrité du toit, à réduire les risques de sinistres coûteux et à maintenir la valeur patrimoniale du bien immobilier. Intégré dans une stratégie régulière de surveillance, il devient un levier essentiel de prévention et de gestion durable.',
+  },
+  footer: {
+    title: 'Votre analyse Birdia près de chez vous',
+    description: 'Découvrez nos offres disponibles dans les villes suivantes :',
+    link: [
+      { name: 'paris', route: '/paris' },
+      { name: 'Lille', route: '/lille' },
+      { name: 'Lyon', route: '/Lyon' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+    ],
+  },
+};
