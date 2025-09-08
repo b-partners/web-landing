5cb7650d641cc641015436858b269f573f9f00ba
Sophie-94 <dasilvapereira@live.fr>
2025-09-08 08:41:26 +0200

    chore: Add page /diagnostics-imobiliers-amiante-toiture with JSON content

---
+++ src/pages/template/json-data/diagnostics-imobiliers-amiante-toiture.ts
@@ -0,0 +1,97 @@
+export default {
+  route: 'paris',
+  metaTitle: 'Diagnostics immobiliers et amiante en toiture : un contrôle réglementaire indispensable',
+  metaDescription:
+    'Découvrez pourquoi les diagnostics immobiliers et amiante en toiture sont essentiels, leurs étapes et leurs bénéfices pour sécuriser toute transaction immobilière.',
+  hero: {
+    title: 'Diagnostics immobiliers et amiante en toiture : un contrôle réglementaire indispensable',
+    items: [
+      'Les diagnostics immobiliers et amiante en toiture permettent d’identifier la présence de fibres dangereuses.',
+      'Obligatoires pour les bâtiments construits avant juillet 1997, ils protègent acquéreurs et occupants.',
+      'Ils conditionnent la vente, la location ou la rénovation d’un bien immobilier.',
+    ],
+  },
+  testimonial: { title1: 'Témoignages clients', title2: 'Passez à l’analyse intelligente, sans compléxité' },
+  testimonials: [
+    {
+      name: 'Arnaud P.',
+      star: 5,
+      content:
+        '"L’outil Birdia est simple, rapide et fiable. Je peux générer un rapport technique avant même de visiter le chantier. Cela me fait gagner des heures chaque semaine, tout en améliorant la qualité de mes prestations."',
+      job: 'Gérant entreprise de couverture (Cannes)',
+    },
+    {
+      star: 5,
+      name: 'Joël D.',
+      content:
+        'Birdia, c’est mon assistant digital. Je l’utilise au quotidien pour détecter les toits à rénover, et les clients adorent recevoir un rapport visuel clair. Même mes devis sont mieux acceptés grâce à ça.',
+      job: 'Couvreur expérimenté (Hauts-de-France)',
+    },
+    {
+      name: 'Laurent M.',
+      star: 5,
+      content:
+        'Birdia est un véritable game changer. L’outil est à la fois intuitif, puissant et ludique. En quelques clics, nous pouvons visualiser, détecter et évaluer des centaines de toitures. Cela change notre manière de penser la prévention et le service client.',
+      job: 'Responsable Innovation – Assureur Habitation',
+    },
+    {
+      star: 5,
+      name: 'Émilie C.',
+      content:
+        'Grâce à Birdia, nos équipes disposent d’une vision détaillée de chaque bien assuré. La précision de l’analyse des matériaux, de l’usure et des risques nous permet d’adapter finement les contrats. C’est un vrai plus pour la maîtrise technique et commerciale.',
+      job: 'Responsable Souscription Habitation – Groupe IARD',
+    },
+  ],
+  analyse: {
+    title: 'Analyse automatisée de toitures par intelligence artificielle',
+    information: [
+      {
+        title: 'Pourquoi les diagnostics immobiliers et amiante en toiture sont essentiels ?',
+        description: 'Ils garantissent la sécurité sanitaire et la conformité légale lors des transactions immobilières.',
+      },
+      {
+        title: 'Comment se déroulent les diagnostics immobiliers et amiante en toiture ?',
+        description: 'Prélèvements, analyses en laboratoire certifié et établissement d’un rapport détaillé officiel.',
+      },
+      {
+        title: 'Que faire après des diagnostics immobiliers et amiante en toiture ?',
+        description: 'Mettre en place un suivi, prévoir un retrait sécurisé ou planifier une gestion adaptée.',
+      },
+    ],
+    explication:
+      ' Détection, qualification et recommandation à partir d’images aériennes HD. En un clic, obtenez un diagnostic métier précis sans monter sur le toit.',
+  },
+  theyTrustUs: { title: 'Ils nous font confiance' },
+  faq: {
+    title1: 'FAQ',
+    q1: { question: 'Question 1', answer: 'Réponse 1' },
+    q2: { question: 'Question 2', answer: 'Réponse 2' },
+    q3: { question: 'Question 3', answer: 'Réponse 3' },
+    q4: { question: 'Question 4', answer: 'Réponse 4' },
+    q5: { question: 'Question 5', answer: 'Réponse 5' },
+    q6: { question: 'Question 6', answer: 'Réponse 6' },
+    title2: 'Vous êtes un particulier , vous souhaitez trouver un couvreur ?',
+  },
+  roofDiagnostics: {
+    title: 'Comprendre les enjeux long terme de diagnostics immobiliers et amiante en toiture',
+    description1:
+      'Les diagnostics immobiliers et amiante en toiture jouent un rôle majeur dans la gestion patrimoniale et la santé publique. L’amiante, longtemps utilisé dans les plaques de fibrociment, présente des risques sanitaires graves en cas de dégradation ou de travaux. Ce diagnostic permet d’identifier les matériaux concernés et de définir les mesures de précaution nécessaires. ',
+    description2:
+      'À long terme, il sécurise les occupants, évite les litiges lors de ventes ou locations et garantit le respect des obligations réglementaires. Il s’agit d’un outil essentiel pour anticiper et maîtriser les risques liés aux toitures anciennes.',
+  },
+  footer: {
+    title: 'Votre analyse Birdia près de chez vous',
+    description: 'Découvrez nos offres disponibles dans les villes suivantes :',
+    link: [
+      { name: 'paris', route: '/paris' },
+      { name: 'Lille', route: '/lille' },
+      { name: 'Lyon', route: '/Lyon' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+      { name: 'paris', route: '/paris' },
+    ],
+  },
+};
