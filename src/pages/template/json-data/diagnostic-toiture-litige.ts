2a04be3d453dd9bb31e84a5e11a603b3e4995fec
RAMANANTSIRESY Amour Bien Aimé <95756837+AmourRamanantsiresy@users.noreply.github.com>
2025-10-10 16:34:52 +0300

    Update page /diagnostic-toiture-litige with JSON content

--- src/pages/template/json-data/diagnostic-toiture-litige.ts
+++ src/pages/template/json-data/diagnostic-toiture-litige.ts
@@ -64,12 +64,30 @@ export default {
   theyTrustUs: { title: 'Ils nous font confiance' },
   faq: {
     title1: 'FAQ',
-    q1: { question: 'Question 1', answer: 'Réponse 1' },
-    q2: { question: 'Question 2', answer: 'Réponse 2' },
-    q3: { question: 'Question 3', answer: 'Réponse 3' },
-    q4: { question: 'Question 4', answer: 'Réponse 4' },
-    q5: { question: 'Question 5', answer: 'Réponse 5' },
-    q6: { question: 'Question 6', answer: 'Réponse 6' },
+    q1: {
+      question: 'Qu’est-ce qu’un diagnostic toiture dans le cadre d’un litige ?',
+      answer: ' C’est une expertise technique indépendante qui permet de déterminer les causes exactes d’un désordre ou d’une fuite sur la toiture.',
+    },
+    q2: {
+      question: 'Quand faut-il demander un diagnostic toiture en cas de litige ?',
+      answer: ' Dès l’apparition d’un différend avec un artisan, un vendeur ou une assurance concernant des travaux ou des infiltrations.',
+    },
+    q3: {
+      question: 'Qui peut réaliser un diagnostic toiture en situation de litige ?',
+      answer: ' Un expert en toiture ou un diagnostiqueur indépendant, habilité à rédiger un rapport objectif utilisable en médiation ou en justice.',
+    },
+    q4: {
+      question: 'Que contient un rapport de diagnostic toiture pour litige ?',
+      answer: 'Il présente les constats techniques, l’origine du problème, les responsabilités possibles et les préconisations de réparation.',
+    },
+    q5: {
+      question: 'Le diagnostic toiture a-t-il une valeur légale en cas de litige ?',
+      answer: ' Oui, s’il est réalisé par un expert certifié, son rapport peut être utilisé comme pièce justificative dans un dossier juridique.',
+    },
+    q6: {
+      question: 'Pourquoi faire appel à Birdia pour un diagnostic toiture en cas de litige ?',
+      answer: ' Birdia propose une inspection toiture par drone, précise et visuelle, idéale pour appuyer un dossier technique ou un constat contradictoire.',
+    },
     title2: 'Vous êtes un particulier , vous souhaitez trouver un couvreur ?',
   },
   roofDiagnostics: {
@@ -83,15 +101,15 @@ export default {
     title: 'Votre analyse Birdia près de chez vous',
     description: 'Découvrez nos offres disponibles dans les villes suivantes :',
     link: [
-      { name: 'paris', route: '/paris' },
-      { name: 'Lille', route: '/lille' },
-      { name: 'Lyon', route: '/Lyon' },
-      { name: 'paris', route: '/paris' },
-      { name: 'paris', route: '/paris' },
-      { name: 'paris', route: '/paris' },
-      { name: 'paris', route: '/paris' },
-      { name: 'paris', route: '/paris' },
-      { name: 'paris', route: '/paris' },
+      { name: 'Lyon', route: '/charpente-lyon' },
+      { name: 'Essone', route: '/diagnistic-toiture-essonne' },
+      { name: 'Evry', route: '/evry' },
+      { name: 'Ile de France', route: '/ile-de-france' },
+      { name: 'Marseille', route: '/devis-diagnostic-marseille' },
+      { name: 'Lannion', route: '/diagnostic-toiture-lannion' },
+      { name: 'Rhone', route: '/diagnostic-rhone' },
+      { name: 'Alsace', route: '/diagnostic-toiture-alsace' },
+      { name: 'Bourges', route: '/BourgesBourges' },
     ],
   },
 };
