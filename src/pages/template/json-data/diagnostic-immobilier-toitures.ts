fcda4bae757871d6f25fd253a8ebe0378c7b49fe
RAMANANTSIRESY Amour Bien Aimé <95756837+AmourRamanantsiresy@users.noreply.github.com>
2025-10-31 02:12:17 +0300

    Update page /diagnostic-immobilier-toitures with JSON content

--- src/pages/template/json-data/diagnostic-immobilier-toitures.ts
+++ src/pages/template/json-data/diagnostic-immobilier-toitures.ts
@@ -63,12 +63,35 @@ export default {
   theyTrustUs: { title: 'Ils nous font confiance' },
   faq: {
     title1: 'FAQ',
-    q1: { question: 'Question 1', answer: 'Réponse 1' },
-    q2: { question: 'Question 2', answer: 'Réponse 2' },
-    q3: { question: 'Question 3', answer: 'Réponse 3' },
-    q4: { question: 'Question 4', answer: 'Réponse 4' },
-    q5: { question: 'Question 5', answer: 'Réponse 5' },
-    q6: { question: 'Question 6', answer: 'Réponse 6' },
+    q1: {
+      question: 'Qu’est-ce qu’un diagnostic immobilier de toiture ?\n',
+      answer:
+        ' Un diagnostic immobilier de toiture est une évaluation technique de l’état du toit réalisée avant la vente, l’achat ou la mise en location d’un bien.',
+    },
+    q2: {
+      question: ' Le diagnostic immobilier de toiture est-il obligatoire ?',
+      answer: ' Contrairement au diagnostic amiante ou au diagnostic énergétique (DPE), le diagnostic de toiture n’est pas obligatoire en France.',
+    },
+    q3: {
+      question: 'Que vérifie un diagnostic immobilier toiture ?',
+      answer:
+        ' Ce diagnostic examine l’état des matériaux de couverture (tuiles, ardoises, zinc, etc.), l’étanchéité, les évacuations pluviales, la charpente et l’isolation sous toiture.',
+    },
+    q4: {
+      question: 'Qui peut réaliser un diagnostic immobilier de toiture ?',
+      answer:
+        ' Le diagnostic doit être réalisé par un expert en inspection toiture ou un technicien indépendant disposant des compétences techniques et des équipements adaptés.',
+    },
+    q5: {
+      question: 'Combien coûte un diagnostic immobilier toiture ?',
+      answer:
+        ' Le tarif varie selon la surface, la complexité du toit et les technologies employées. En moyenne, le coût d’un diagnostic toiture se situe entre 150 et 400 €.',
+    },
+    q6: {
+      question: 'Pourquoi faire appel à Birdia pour un diagnostic immobilier de toiture ?',
+      answer:
+        ' Birdia propose un service de diagnostic toiture par drone, indépendant et non intrusif. Cette approche offre une vision complète de l’état du toit grâce à des images haute définition et des analyses thermiques.',
+    },
     title2: 'Vous êtes un particulier , vous souhaitez trouver un couvreur ?',
   },
   roofDiagnostics: {
@@ -82,15 +105,15 @@ export default {
     title: 'Votre analyse Birdia près de chez vous',
     description: 'Découvrez nos offres disponibles dans les villes suivantes :',
     link: [
-      { name: 'paris', route: '/paris' },
-      { name: 'Lille', route: '/lille' },
-      { name: 'Lyon', route: '/Lyon' },
-      { name: 'paris', route: '/paris' },
-      { name: 'paris', route: '/paris' },
-      { name: 'paris', route: '/paris' },
-      { name: 'paris', route: '/paris' },
-      { name: 'paris', route: '/paris' },
-      { name: 'paris', route: '/paris' },
+      { name: 'Lyon', route: '/charpente-lyon' },
+      { name: 'Essone', route: '/diagnistic-toiture-essonne' },
+      { name: 'Evry', route: '/evry' },
+      { name: 'Ile de France', route: '/ile-de-france' },
+      { name: 'Marseille', route: '/devis-diagnostic-marseille' },
+      { name: 'Lannion', route: '/diagnostic-toiture-lannion' },
+      { name: 'Rhone', route: '/diagnostic-rhone' },
+      { name: 'Alsace', route: '/diagnostic-toiture-alsace' },
+      { name: 'Bourges', route: '/BourgesBourges' },
     ],
   },
 };
