9e52ef9dd111241a759a0c6480ad8ddaca8ba11e
kirioka <hei.amour.2@gmail.com>
2025-10-31 04:53:15 +0300

    chore: implement directory template page

--- src/pages/Directory/directory.tsx
+++ src/pages/Directory/directory.tsx
@@ -1,7 +1,187 @@
-import { Box } from '@mui/material';
+import { FC, useState } from 'react';
+import { Circle, MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
+
+import { LocationOn } from '@mui/icons-material';
+import { Box, Button, Chip, Grid, Stack, Typography } from '@mui/material';
+import { Icon } from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 import { directoryStyle as style } from './styles';
 
+const userIcon = new Icon({
+  iconUrl: 'https://cdn-icons-png.flaticon.com/512/447/447031.png',
+  iconSize: [38, 38],
+});
+
+const ChangeLocation: FC<{ coordinates: [number, number] }> = (props) => {
+  const map = useMap();
+  map.setView(props.coordinates);
+  return null;
+};
+
+const parisLocation = [48.8581479079253, 2.342225169981041];
+const houilleLocation = [48.92680381284064, 2.1887642439036217];
 export const Directory = () => {
-  return <Box sx={style}></Box>;
+  const [location, setLocation] = useState<any>(parisLocation as any);
+
+  return (
+    <Box sx={style}>
+      <Box className="hero">
+        <Box className="hero-left">
+          <Stack gap={1} className="hero-left-container">
+            <Typography variant="h2" fontWeight="bold" color="white">
+              Toitures Robert
+            </Typography>
+            <Typography color="white" variant="h4">
+              SIRET 75119619700022
+            </Typography>
+            <Box>
+              <Button variant="contained">Demander un devis</Button>
+            </Box>
+          </Stack>
+        </Box>
+        <Box className="hero-right">
+          <Stack className="hero-right-container" gap={1}>
+            <Typography variant="h4" className="title">
+              Vous êtes couvreur ?
+            </Typography>
+            <Typography>
+              Investissez dans une solution intelligente qui automatise l'analyse des images aériennes pour obtenir des évaluations fiables des toitures,
+              végétations ou structures.
+            </Typography>
+            <Box className="button-container">
+              <Button color="secondary" variant="outlined">
+                Testez gratuitement Birdia
+              </Button>
+            </Box>
+          </Stack>
+        </Box>
+      </Box>
+      <Stack direction="row" mt="1rem" className="content" gap={2} position="relative">
+        <Box maxWidth={{ xs: '100%', md: '82vw' }} flexGrow={1} className="content-left">
+          <Box className="content-activity">
+            <Typography fontWeight="bold" variant="h4">
+              Activité
+            </Typography>
+            <Stack direction="row" gap={1} mt={1}>
+              <Chip color="primary" label="Construction" />
+              <Chip color="primary" label="Couvreurs" />
+            </Stack>
+          </Box>
+          <Box className="content-map-container">
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={4}>
+                <Box className="content-map-actions">
+                  <Typography fontWeight="bold" variant="h4">
+                    Zone d'intervention
+                  </Typography>
+                  <Stack direction="row">
+                    <LocationOn />
+                    <Typography component="a">83 r Égalités, 78800 Houilles</Typography>
+                  </Stack>
+                  <Typography variant="h4" mt={2}>
+                    Villes d'interventions :
+                  </Typography>
+                  <Stack direction="row" flexWrap="wrap" gap={1}>
+                    <Button onClick={() => setLocation(houilleLocation)} color="secondary" variant="outlined">
+                      Houilles
+                    </Button>
+                    <Button onClick={() => setLocation(houilleLocation)} color="secondary" variant="outlined">
+                      Houilles
+                    </Button>
+                    <Button onClick={() => setLocation(houilleLocation)} color="secondary" variant="outlined">
+                      Houilles
+                    </Button>
+                    <Button onClick={() => setLocation(houilleLocation)} color="secondary" variant="outlined">
+                      Paris
+                    </Button>
+                    <Button onClick={() => setLocation(parisLocation)} color="secondary" variant="outlined">
+                      Paris
+                    </Button>
+                    <Button onClick={() => setLocation(parisLocation)} color="secondary" variant="outlined">
+                      Paris
+                    </Button>
+                    <Button onClick={() => setLocation(parisLocation)} color="secondary" variant="outlined">
+                      Paris
+                    </Button>
+                    <Button onClick={() => setLocation(parisLocation)} color="secondary" variant="outlined">
+                      Paris
+                    </Button>
+                    <Button onClick={() => setLocation(parisLocation)} color="secondary" variant="outlined">
+                      Paris
+                    </Button>
+                  </Stack>
+                </Box>
+              </Grid>
+              <Grid item xs={12} md={8}>
+                <Box className="content-map">
+                  <MapContainer center={parisLocation as any} zoom={13} scrollWheelZoom={false} style={{ height: '41vh' }}>
+                    <TileLayer
+                      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Circle center={location} radius={1000} color="blue" opacity={0.2} />
+                    <ChangeLocation coordinates={location} />
+                    <Marker position={location as any} icon={userIcon}>
+                      <Popup>Zone d'intervention</Popup>
+                    </Marker>
+                  </MapContainer>
+                </Box>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Box width="20vw" className="content-right">
+          <Box className="content-schedules">
+            <Typography fontWeight="bold" variant="h4">
+              Horaires
+            </Typography>
+            <Stack mt={2}>
+              <Box className="bg-neon-light">
+                <Typography>Lundi</Typography>
+                <Typography>08h-19h</Typography>
+              </Box>
+              <Box>
+                <Typography>Mardi</Typography>
+                <Typography>08h-19h</Typography>
+              </Box>
+              <Box className="bg-neon-light">
+                <Typography>Mercredi</Typography>
+                <Typography>08h-19h</Typography>
+              </Box>
+              <Box>
+                <Typography>Jeudi</Typography>
+                <Typography>08h-19h</Typography>
+              </Box>
+              <Box className="bg-neon-light">
+                <Typography>Vendredi</Typography>
+                <Typography>08h-19h</Typography>
+              </Box>
+              <Box>
+                <Typography>Samedi</Typography>
+                <Typography>08h-12h</Typography>
+              </Box>
+              <Box className="bg-neon-light">
+                <Typography>Dimanche</Typography>
+                <Typography>Fermé</Typography>
+              </Box>
+            </Stack>
+          </Box>
+          <Box className="content-other-informations">
+            <Typography fontWeight="bold" variant="h4">
+              Autres Informations
+            </Typography>
+            <Typography mt={1} fontWeight="bold" variant="h6">
+              Types de prestations
+            </Typography>
+            <Typography>Déplacement chez le client</Typography>
+            <Typography mt={1} fontWeight="bold" variant="h6">
+              Éligible service à la personne
+            </Typography>
+            <Typography>Non</Typography>
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
+  );
 };
--- src/pages/Directory/styles.ts
+++ src/pages/Directory/styles.ts
@@ -1,5 +1,109 @@
+import { PALETTE_COLORS } from '@/config/theme';
 import { SxProps } from '@mui/material';
 
 export const directoryStyle: SxProps = {
   minHeight: '100vh',
+  background: '#fff',
+  position: 'relative',
+  '& .hero': {
+    minHeight: '40vh',
+    width: '100%',
+    borderRadius: '0 0 3rem 3rem',
+    background: PALETTE_COLORS['pine'],
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+    py: 3,
+  },
+  '& .hero > .MuiBox-root': {
+    transform: { md: 'translateY(10%)' },
+  },
+  '& .hero-left': {
+    width: { xs: '100%', md: '50%' },
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  '& .hero-left-container .MuiButton-root': {
+    mt: 3,
+  },
+  '& .hero-right': {
+    width: { xs: '100%', md: '50%' },
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    mt: { xs: 2, md: 0 },
+  },
+  '& .hero-right-container': {
+    mt: { xs: 2, md: 0 },
+    width: {
+      xs: '90%',
+      sm: '80%',
+    },
+    padding: '2rem',
+    background: PALETTE_COLORS['cream'],
+    borderRadius: '3rem',
+  },
+  '& .hero-right-container .title': {
+    color: PALETTE_COLORS['neon_orange'],
+  },
+  '& .hero-right-container .button-container': {
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    fontSize: '10rem',
+  },
+  '& .hero .MuiButton-root': {
+    fontSize: '1rem',
+  },
+  '& .content': {
+    p: 2,
+  },
+  '& .content-schedules': {
+    p: 3,
+    borderRadius: 5,
+    background: PALETTE_COLORS['cream'],
+  },
+  '& .content-schedules > .MuiStack-root': {
+    background: PALETTE_COLORS['cream'],
+    borderRadius: 3,
+    overflow: 'hidden',
+  },
+  '& .content-schedules .MuiStack-root > .MuiBox-root': {
+    display: 'flex',
+    justifyContent: 'space-between',
+    py: 0.5,
+    px: 2,
+  },
+  '& .content-schedules .MuiStack-root > .bg-neon-light': {
+    background: PALETTE_COLORS['neon_orange_light'],
+  },
+  '& .content-other-informations': {
+    mt: 2,
+    p: 3,
+    borderRadius: 5,
+    background: PALETTE_COLORS['cream'],
+  },
+  '& .content-activity': {
+    p: 3,
+    borderRadius: 5,
+    background: PALETTE_COLORS['cream'],
+  },
+  '& .content-map-container': {
+    mt: 2,
+    p: 3,
+    borderRadius: 5,
+    background: PALETTE_COLORS['cream'],
+    minHeight: '41vh',
+    position: 'relative',
+  },
+  '& .content-right': {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+  },
+  '& .content-right > MuiBox-root': {
+    flexGrow: 1,
+  },
 };
